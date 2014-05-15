e30c2d77775b4861704aff02af7241ae88881b36
Paul Gilzow <gilzowp@missouri.edu>
2014-05-15 16:21:49 -0500

    forgot a div

--- ipp/views/policy-area.php
+++ ipp/views/policy-area.php
@@ -15,6 +15,59 @@
  * You do not need to call get_header() and get_footer
  */
 ?>
+<div class="flex span7">
+    <?php breadcrumbs(); //@todo this needs to moved out of here. REFACTOR. ?>
+    <main id="main" role="main">
+        <div id="content">
+            <article role="article">
+                <header>
+                    <h1 id="title"><?php echo $objMainPost->title; ?></h1>
+                </header>
+                <section aria-label="content" role="region">
+                    <?php echo $objMainPost->content; ?>
+                </section>
+
+
+
+                <?php if (count($aryRelatedPublications) > 0) : ?>
+                    <section>
+                        <h3>Related Publications:</h3>
+                        <ul>
+                            <?php foreach ($aryRelatedPublications as $objPublication) : ?>
+                                <li><a href="<?php echo $objPublication->permalink; ?>" title="Link to <?php echo $objPublication->title; ?>"><?php echo $objPublication->title; ?></a></li>
+                            <?php endforeach;?>
+                        </ul>
+                    </section>
+                <?php endif; ?>
+
+                <?php if (count($aryRelatedProjects) > 0) : ?>
+                    <section>
+                        <h3>Related Projects:</h3>
+                        <ul>
+                            <?php foreach ($aryRelatedProjects as $objProject) : ?>
+                                <li><a href="<?php echo $objProject->permalink; ?>" title="Link to <?php echo $objProject->title; ?>"><?php echo $objProject->title; ?></a></li>
+                            <?php endforeach;?>
+                        </ul>
+                    </section>
+                <?php endif; ?>
+
+                <?php if(isset($objMainContact) && is_object($objMainContact)): ?>
+                    <section>
+                        <h3><?php echo $objMainPost->title; ?> Contact:</h3>
+                        <p>
+                            Name: <?php echo $objMainContact->title; ?><br>
+                            Title: <?php echo $objMainContact->meta_data->title1; ?><br>
+                            Address: <?php echo $objMainContact->meta_data->address1; ?><br>
+                            Email: <a href="mailto:<?php echo $objMainContact->meta_data->email; ?>"><?php echo $objMainContact->meta_data->email; ?></a><br>
+                            Phone: <?php echo $objMainContact->meta_data->phone;?>
+                        </p>
+                    </section>
+                <?php endif; ?>
+
+            </article>
+        </div>
+    </main>
+</div>
 <main id="main" role="main">
     <div id="content">
         <article role="article">
@@ -69,5 +122,5 @@
 
 <p>Main Staff Contact:</p>
 <pre>
-
+<?php print_r($objMainContact); ?>
 </pre>
