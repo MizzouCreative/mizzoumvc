6e34d3393f2e06bb8b64200b10557c4dfb4b14ef
Paul Gilzow <gilzowp@missouri.edu>
2014-05-15 14:31:47 -0500

    initial commit

---
+++ ipp/views/policy-area.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+<p>Related Publications:</p>
+<pre>
+    <?php var_export($aryRelatedPublications); ?>
+</pre>
+<p>Related Projects:</p>
+<pre>
+    <?php var_export($aryRelatedProjects);?>
+</pre>
+
+<p>Main Staff Contact:</p>
+<pre>
+
+</pre>
