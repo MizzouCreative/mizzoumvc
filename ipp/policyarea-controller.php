975c94a39573437d9d862b32935813b7c47a5e2d
Paul Gilzow <gilzowp@missouri.edu>
2014-05-15 16:02:18 -0500

    mizzouIppRetrieve contact

--- ipp/policyarea-controller.php
+++ ipp/policyarea-controller.php
@@ -13,38 +13,42 @@
  * @copyright 2014 Curators of the University of Missouri
  */
 
-/**
- * @param $strPostType
- * @param $intCount
- * @param string $strTaxonomy
- * @param string $strTaxTerm
- * @param string $strTaxField
- * @param string $strOrderBy
- * @param string $strOrderDirection
- * @param bool $boolIncludeMeta
- * @param string $strMetaPrefix
- * @return array
- */
-function mizzouRetrieveRelatedContent($strPostType,$intCount=-1,$strTaxonomy='',$strTaxTerm='',$strTaxField='slug',
-        $strOrderBy='date',$strOrderDirection='DESC',$boolIncludeMeta = false,$strMetaPrefix=''
-) {
+
+function mizzouRetrieveRelatedContent($aryOptions) {
+    $aryDefaults = array(
+        'post_type'         => 'post',
+        'count'             => -1,
+        'taxonomy'          => '',
+        'tax_term'          => '',
+        'tax_field'         => 'slug',
+        'complex_tax'       => null,
+        'order_by'          => 'date',
+        'order_direction'   => 'DESC',
+        'include_meta'      => false,
+        'meta_prefix'       => ''
+    );
+
+    $aryOptions = array_merge($aryDefaults,$aryOptions);
+
     $aryReturn = array();
 
     $aryArgs = array(
-        'post_type'     =>  $strPostType,
-        'numberposts'   =>  $intCount,
-        'orderby'       =>  $strOrderBy,
-        'order'         =>  $strOrderDirection
+        'post_type'     =>  $aryOptions['post_type'],
+        'numberposts'   =>  $aryOptions['count'],
+        'orderby'       =>  $aryOptions['order_by'],
+        'order'         =>  $aryOptions['order_direction']
     );
 
-    if('' != $strTaxonomy && '' != $strTaxTerm){
+    if ('' != $aryOptions['taxonomy'] && '' != $aryOptions['tax_term']){
         $aryTaxQuery = array(
-            'taxonomy'  => $strTaxonomy,
-            'field'     => $strTaxField,
-            'terms'     => $strTaxTerm
+            'taxonomy'  => $aryOptions['taxonomy'],
+            'field'     => $aryOptions['tax_field'],
+            'terms'     => $aryOptions['tax_term']
         );
 
         $aryArgs = array_merge($aryArgs,array('tax_query'=>array($aryTaxQuery)));
+    } elseif (is_array($aryOptions['complex_tax']) && !is_null($aryOptions['complex_tax'])) {
+        $aryArgs = array_merge($aryArgs,array('tax_query'=>$aryOptions['complex_tax']));
     }
 
     $objQuery =  new WP_Query($aryArgs);
@@ -52,7 +56,7 @@ function mizzouRetrieveRelatedContent($strPostType,$intCount=-1,$strTaxonomy='',
     if (isset($objQuery->posts) && count($objQuery->posts) > 0){
         foreach($objQuery->posts as $objPost){
             $objMizzouPost = new MizzouPost($objPost);
-            if($boolIncludeMeta){
+            if($aryOptions['include_meta']){
                 $objMizzouPost->meta_data = new PostMetaData($objPost->ID,$strMetaPrefix);
             }
 
@@ -66,14 +70,55 @@ function mizzouRetrieveRelatedContent($strPostType,$intCount=-1,$strTaxonomy='',
 
 function mizzouIppRetrieveRelatedPublications($strTerm)
 {
-    $intNumber = 4; //this needs to be retrieved from config variable or theme option
-    return mizzouRetrieveRelatedContent('publication',$intNumber,'policy_area',$strTerm);
+    $aryArgs = array(
+        'post_type' => 'publication',
+        'count'     => 4, //this needs to be retrieved from config variable or theme option
+        'taxonomy'  => 'policy_area',
+        'tax_term'  => $strTerm
+    );
+    return mizzouRetrieveRelatedContent($aryArgs);
 }
 
 function mizzouIppRetrieveRelatedProjects($strTerm)
 {
-    $intNumber = 4; //this needs to be retrieved from config variable or theme option
-    return mizzouRetrieveRelatedContent('project',$intNumber,'policy_area',$strTerm);
+    $aryArgs = array(
+        'post_type' => 'project',
+        'count'     => 4, //@todo this needs to be retrieved from config variable or theme option
+        'taxonomy'  => 'policy_area',
+        'tax_term'  => $strTerm
+    );
+    return mizzouRetrieveRelatedContent($aryArgs);
+}
+
+function mizzouIppRetrieveContact($strTerm)
+{
+    $aryTax = array(
+        'relation'  => 'AND',
+        array(
+            'taxonomy'  => 'policy_area',
+            'field'     => 'slug',
+            'terms'     => $strTerm
+        ),
+        array(
+            'taxonomy'  => 'person_type', //defined in Mizzou People plugin
+            'field'     => 'slug',
+            'terms'     => 'lead-analyst' //@todo this should be moved into config or theme option
+        )
+    );
+
+    $aryArgs = array(
+        'post_type'     => 'person',
+        'count'         => 1, //@todo this needs to be retrieved from config variable or theme option
+        'complex_tax'   => $aryTax,
+        'include_meta'  => true,
+        'meta_prefix'   => 'person_'//@todo needs to be rerieved, not hardcoded
+    );
+
+    $aryMatches = mizzouRetrieveRelatedContent($aryArgs);
+
+    if(count($aryMatches) !== 1) //@todo throw an exception, log it, something!
+
+    return $aryMatches[0];
 }
 
 /**
@@ -84,10 +129,13 @@ function mizzouIppRetrieveRelatedProjects($strTerm)
  * 4 publications, link to all pubs
  */
 
-$strPageContent = apply_filters('the_content',get_the_content());
+$objMainPost = new MizzouPost($post);
 $aryRelatedPublications = mizzouIppRetrieveRelatedPublications('education');
 $aryRelatedProjects = mizzouIppRetrieveRelatedProjects('education');
+$objMainContact = mizzouIppRetrieveContact('education');
 
 get_header();
+get_sidebar();
+getbreadcrumbs(); //this is from Mizzou Breadcrumbs plugin.
 require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR.'policy-area.php';
 get_footer();
