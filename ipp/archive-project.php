29448ced89bacdc05c1a8ae4442f7cb4df9084f7
Paul Gilzow <gilzowp@missouri.edu>
2014-05-16 15:24:02 -0500

    test

--- ipp/archive-project.php
+++ ipp/archive-project.php
@@ -9,3 +9,14 @@
  * @author Paul Gilzow, Web Communications, University of Missouri
  * @copyright 2014 Curators of the University of Missouri
  */
+
+/**
+ * get our model
+ * @todo we should be able to make this a function and move up higher
+ */
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'models'.DIRECTORY_SEPARATOR.'project.php';
+//@todo move this up higher as well
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'models'.DIRECTORY_SEPARATOR.'viewOutput.php';
+$aryData = array();
+
+var_export($post);
