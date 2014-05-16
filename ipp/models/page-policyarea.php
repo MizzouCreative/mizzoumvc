b5b7d56baf73e0a3b3d2de21337f2866c9d5fc06
Paul Gilzow <gilzowp@missouri.edu>
2014-05-16 09:16:19 -0500

    moved model functions from controller to here

---
+++ ipp/models/page-policyarea.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ */
+
+/**
+ * @param $strTerm
+ * @return array
+ */
+function mizzouIppRetrieveRelatedPublications($strTerm)
+{
+    $aryArgs = array(
+        'post_type' => 'publication',
+        'count'     => 4, //this needs to be retrieved from config variable or theme option
+        'taxonomy'  => 'policy_area',
+        'tax_term'  => $strTerm
+    );
+    return mizzouRetrieveRelatedContent($aryArgs);
+}
+
+/**
+ * @param $strTerm
+ * @return array
+ */
+function mizzouIppRetrieveRelatedProjects($strTerm)
+{
+    $aryArgs = array(
+        'post_type' => 'project',
+        'count'     => 4, //@todo this needs to be retrieved from config variable or theme option
+        'taxonomy'  => 'policy_area',
+        'tax_term'  => $strTerm
+    );
+    return mizzouRetrieveRelatedContent($aryArgs);
+}
+
+/**
+ * @param $strTerm
+ * @return mixed
+ */
+function mizzouIppRetrieveContact($strTerm)
+{
+    $aryTax = array(
+        'relation'  => 'AND',
+        array(
+            'taxonomy'  => 'policy_area',
+            'field'     => 'slug',
+            'terms'     => $strTerm
+        ),
+        array(
+            'taxonomy'  => 'person_type', //defined in Mizzou People plugin
+            'field'     => 'slug',
+            'terms'     => 'lead-analyst' //@todo this should be moved into config or theme option
+        )
+    );
+
+    $aryArgs = array(
+        'post_type'     => 'person',
+        'count'         => 1, //@todo this needs to be retrieved from config variable or theme option
+        'complex_tax'   => $aryTax,
+        'include_meta'  => true,
+        'meta_prefix'   => 'person_'//@todo needs to be rerieved, not hardcoded
+    );
+
+    $aryMatches = mizzouRetrieveRelatedContent($aryArgs);
+
+    if(count($aryMatches) !== 1) //@todo throw an exception, log it, something!
+        //@todo ask if we should grab some default contact info to display?
+        _mizzou_log($aryMatches,'Array Matches',false,array('func'=>__FUNCTION__));
+    return $aryMatches[0];
+}
