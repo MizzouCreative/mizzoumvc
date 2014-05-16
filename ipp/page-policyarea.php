7563754e5d1a11a3eb838ce9d62d3cdbbb85808d
Paul Gilzow <gilzowp@missouri.edu>
2014-05-16 11:05:57 -0500

    uh, we need our view output controller

--- ipp/page-policyarea.php
+++ ipp/page-policyarea.php
@@ -27,10 +27,11 @@
 
 /**
  * get our model
- * @todo move this higher up
+ * @todo we should be able to make this a function and move up higher
  */
 require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'models'.DIRECTORY_SEPARATOR.basename(__FILE__);
-
+//@todo move this up higher as well
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'models'.DIRECTORY_SEPARATOR.'viewOutput.php';
 $aryData = array();
 $aryData['objMainPost'] = new MizzouPost($post);
 $aryData['objMainContact'] = mizzouIppRetrieveContact($post->post_name);
