46d3c452e60267188e5482fa5eac62a974b8ea13
Paul Gilzow <gilzowp@missouri.edu>
2014-05-16 10:55:30 -0500

    links to project and publication archive URL

--- ipp/views/policy-area.php
+++ ipp/views/policy-area.php
@@ -1,18 +1,25 @@
 <?php
 /**
+ * Inner view file for Policy Area pages
+ *
  * From the IA, this view needs to display the following data:
  *  - body content has entered into wordpress for this page
  *  - Related Publications
+ *  - Link to all Publications
  *  - Related Projects
- *  - Main Staff Contact
+ *  - Link to all Projects
+ *  - Main Staff Contact for a Policy Area
  *
  * To facilitate this, you have access to the following variables:
  *  - $objMainPost Post object for the page
  *  - $aryRelatedPublications list/array of Publication Post objects that match
+ *  - $strPublicationArchiveURL the url for the publication CPT archive
  *  - $aryRelatedProjects list/array of Project Post objects that match
+ *  - $strProjectArchiveURL the url for the project CPT archive
  *  - $objMainContact Person Post object that matches
  *
- * You do not need to call get_header() and get_footer
+ * You do not need to call get_header(), get_footer, get_sidebar() or breadcrumbs() as those are handled by outer
+ * functions.
  */
 ?>
     <main id="main" role="main">
@@ -25,8 +32,6 @@
                     <?php echo $objMainPost->content; ?>
                 </section>
 
-
-
                 <?php if (count($aryRelatedPublications) > 0) : ?>
                     <section>
                         <h3>Related Publications:</h3>
@@ -35,6 +40,7 @@
                                 <li><a href="<?php echo $objPublication->permalink; ?>" title="Link to <?php echo $objPublication->title; ?>"><?php echo $objPublication->title; ?></a></li>
                             <?php endforeach;?>
                         </ul>
+                        <p><a href="<?php echo $strPublicationArchiveURL; ?>" title="Link to all Publications">All Publications</a> </p>
                     </section>
                 <?php endif; ?>
 
@@ -46,6 +52,7 @@
                                 <li><a href="<?php echo $objProject->permalink; ?>" title="Link to <?php echo $objProject->title; ?>"><?php echo $objProject->title; ?></a></li>
                             <?php endforeach;?>
                         </ul>
+                        <p><a href="<?php echo $strProjectArchiveURL; ?>" title="Link to all Projects">All Projects</a> </p>
                     </section>
                 <?php endif; ?>
 
