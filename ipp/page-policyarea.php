4761d22aa71bc55cc148f3cafec7d69c861af3c1
Paul Gilzow <gilzowp@missouri.edu>
2014-05-20 15:45:05 -0500

    updated controller to reflect change in model

--- ipp/page-policyarea.php
+++ ipp/page-policyarea.php
@@ -18,7 +18,7 @@
 
 /**
  * Data needed by the view
- * breadcrumbs
+ * breadcrumbs - handled by OutPutView
  * default content for the page
  * 4 related projects, link to all projects
  * 1 main contact
@@ -32,10 +32,18 @@
 require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'models'.DIRECTORY_SEPARATOR.basename(__FILE__);
 //@todo move this up higher as well
 require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'models'.DIRECTORY_SEPARATOR.'viewOutput.php';
+
+$objPageModel = new PolicyArea(); // our model object
+
+//data array
 $aryData = array();
+//gather the necessary data
 $aryData['objMainPost'] = new MizzouPost($post);
-$aryData['objMainContact'] = mizzouIppRetrieveContact($post->post_name);
-mizzouRetrieveProjectData($post->post_name,$aryData);
-mizzouRetrievePublicationData($post->post_name,$aryData);
+$aryData['objMainContact'] = $objPageModel->retrieveContact($post->post_name);
+$aryData['aryRelatedProjects'] = $objPageModel->retrieveProjects($post->post_name);
+$aryData['strProjectArchiveURL'] = $objPageModel->retrieveProjectsArchivePermalink();
+$aryData['aryRelatedPublications'] = $objPageModel->retrievePublications($post->post_name);
+$aryData['strPublicationArchiveURL'] = $objPageModel->retrievePublicationsArchivePermalink();
+
 
 mizzouOutPutView('policy-area',$aryData);
