2ade6a3ae2fa4404eab030aebd55c43fc2efce11
Paul Gilzow <gilzowp@missouri.edu>
2014-05-20 15:45:38 -0500

    updated model to reflect change in parent class

--- ipp/models/publication.php
+++ ipp/models/publication.php
@@ -4,4 +4,16 @@
  * User: Frank
  * Date: 5/16/14
  * Time: 3:10 PM
- */ 
+ */
+
+require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'base.php';
+
+class Publication extends WpBase
+{
+    /**
+     * Overload the parent member
+     * @var string
+     */
+    protected $strPostType = 'publication';
+
+}
