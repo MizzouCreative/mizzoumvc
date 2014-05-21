a1c61c45a55bc87c0bc1c75db2b9051090c6448f
Paul Gilzow <gilzowp@missouri.edu>
2014-05-21 12:28:19 -0500

    dump contents from topstaff specifically to see how wp_query handles order in a complex meta query.

--- ipp/page-staff.php
+++ ipp/page-staff.php
@@ -5,5 +5,9 @@
  * Date: 5/20/14
  * Time: 4:47 PM
  */
+
+require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'model'.DIRECTORY_SEPARATOR.'people.php';
+
+$objPeople = new People();
+var_export($objPeople->retrieveTopStaff());
 ?>
-<p>Staff</p>
