916f214f1bd90953b5f67b0e9c2a99abf3edea79
Paul Gilzow <gilzowp@missouri.edu>
2014-05-21 12:30:31 -0500

    i hate typos

--- ipp/page-staff.php
+++ ipp/page-staff.php
@@ -6,7 +6,7 @@
  * Time: 4:47 PM
  */
 
-require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'model'.DIRECTORY_SEPARATOR.'people.php';
+require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'models'.DIRECTORY_SEPARATOR.'people.php';
 
 $objPeople = new People();
 var_export($objPeople->retrieveTopStaff());
