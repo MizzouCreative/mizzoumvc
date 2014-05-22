7267328acebd8daabafa366b8ec0e21e3829bed0
Paul Gilzow <gilzowp@missouri.edu>
2014-05-22 09:09:20 -0500

    remove old code

--- ipp/models/people.php
+++ ipp/models/people.php
@@ -62,78 +62,8 @@ class People extends WpBase
 
     public function retrieveTopStaff()
     {
-        //global $wpdb;
-        //$aryReturn = array();
-        /**
-         * Doing a complex meta query for three titles (with two results) was taking 23.1333 seconds.
-         * Meta query removed
-         *
-         * Doing our own direct query on postmeta to pull IDs that match our titles took 1.6s when using wpdb->prepare.
-         * Removing prepare and using sprintf dropped that to .8333s.
-         *
-         * Taking the list of ids and using post__in took about 1.1s. Looping over each ID and running a query took 0.9s
-         *
-         *
-         */
-
-        /**
-         * One potential problem is that we arent checking if the person is also a staff member. So if they create a
-         * person that is a different person type, but then give them a title that matches our top titles, they'll
-         * still be selected here
-         */
-        /*
-        $strSQL = "SELECT a.post_id,a.meta_value FROM mutspaipp_2.ipp_postmeta a, mutspaipp_2.ipp_posts b
-                    WHERE
-                        a.post_id = b.ID AND
-                        b.post_status = 'publish'
-                    AND
-
-                            a.meta_key = 'person_title1'
-                    AND 	a.meta_value IN (%s);";
-
-        $strTitleVals = "'".implode("','",$this->aryTopStaff)."'";
-        /**
-         * ok, running our query above using $wpdb->prepare was taking 1.6s. Cutting out prepare and doing sprintf
-         * brought that down to 0.833
-         *
-        $strSQL = sprintf($strSQL,$strTitleVals);
-
-
-        $aryTopStaffIDs = $wpdb->get_results($strSQL);
-
-        /**
-         * ok, this next piece might seem confusing. Why are we looping and then running a query over each item, instead
-         * of doing one query with post__in and the array of post ids?  Because, surprisingly, it's actually faster
-         * to loop and query each one separately.  Using post__in took about 1.1 seconds to return the results. Looping'
-         * over each one, as below, takes 0.9s.
-
-        if(is_array($aryTopStaffIDs) && count($aryTopStaffIDs) > 0){
-            //first we need to resort the post_ids into the correct order
-            $aryTopStaffOrdered = array();
-            foreach($aryTopStaffIDs as $objTopStaff){
-                $aryTopStaffOrdered[array_search($objTopStaff->meta_value,$this->aryTopStaff)] = $objTopStaff->post_id;
-            }
-
-            ksort($aryTopStaffOrdered);
-            $this->aryTopStaffIds = $aryTopStaffOrdered;
-
-            foreach($aryTopStaffOrdered as $intPostId){
-                $aryArg = array(
-                    'passthru'=>array('p'=>$intPostId)
-                );
-                //_mizzou_log($aryArg,'aryArg for what should be post_id ' . $objTopStaff->post_id);
-                $aryResults = $this->retrieveContent($aryArg);
-                if(isset($aryResults[0])){
-                    $aryReturn[] = $aryResults[0];
-                }
-            }
-        }
-
-        return $aryReturn;
-
-        */
         $aryReturn = array();
-        //how long does it take if we just do a meta query for each title?
+
         foreach($this->aryTopStaff as $strTitle){
             $aryMeta = array(
                 'key'   => $this->strPostPrefix.'title1',
