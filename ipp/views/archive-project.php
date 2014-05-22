6d52893fd18328f2615ffdcb5aaa8c1ca7f24f95
Paul Gilzow <gilzowp@missouri.edu>
2014-05-22 11:21:46 -0500

    moved markup that was repeating in each view into outerView

--- ipp/views/archive-project.php
+++ ipp/views/archive-project.php
@@ -1,21 +1,9 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Frank
- * Date: 5/16/14
- * Time: 3:36 PM
+ * $strLoopContent comes from views/projects-loop
  */
 ?>
-
-<main id="main" role="main">
-    <div id="content">
-        <article role="article">
-            <header>
-                <h1 id="title"><?php echo $strPageTitle; ?></h1>
-            </header>
             <section aria-label="content" role="region">
                 <?php echo $strLoopContent; ?>
             </section>
-        </article>
-    </div>
-</main>
+
