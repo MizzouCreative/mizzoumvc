128ad7f063a2eda4fe3e07b0060af9808bcf54b5
Paul Gilzow <gilzowp@missouri.edu>
2014-05-22 14:48:54 -0500

    remove logger

--- ipp/models/people.php
+++ ipp/models/people.php
@@ -100,8 +100,6 @@ class People extends WpBase
     protected function _setDefaults()
     {
         parent::_setDefaults();
-        _mizzou_log($this->aryDefaults,'our defaults from parent',false,array('func'=>__FUNCTION__));
         $this->aryDefaults = array_merge($this->aryDefaults,$this->aryPeopleDefaults);
-        _mizzou_log($this->aryDefaults,'our defaults after we merge',false,array('func'=>__FUNCTION__));
     }
 }
