f6a1e206e62ca7a23445b02b76d296d5ff2c2362
Paul Gilzow <gilzowp@missouri.edu>
2014-05-23 13:23:25 -0500

    initial commit

---
+++ ipp/views/archive-publication.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Inner view file for Staff page
+ *
+ * From the IA, this view needs to display the following data:
+ *  - Publication title
+ *  - Publication permalink
+ *
+ * To facilitate this, you have access to the following variables:
+ *  - $objMainPost Main Post object for the page, you shouldnt need it, but just in case you do
+ *  - $aryPublications
+ *
+ * You do not need to call get_header(), get_footer, get_sidebar() or breadcrumbs() as those are handled by outer
+ * functions and/or views.
+ *
+ * @package WordPress
+ * @subpackage IPP
+ * @category theme
+ * @category view
+ * @author Paul Gilzow, Web Communications, University of Missouri
+ * @copyright 2014 Curators of the University of Missouri
+ */
+?>
+<section aria-label="content" role="region">
+    <ol>
+        <?php foreach($aryPublication as $objPublication) : ?>
+        <li><a href="<?php echo $objPublication->permalink; ?>" title="Permanent link for <?php echo $objPublication->title; ?>"></a></li>
+        <?php endforeach; ?>
+    </ol>
+</section>
