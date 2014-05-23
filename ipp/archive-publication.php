de8d1eb01cf1216166c2de56bf63190c2ecca390
Paul Gilzow <gilzowp@missouri.edu>
2014-05-23 13:18:18 -0500

    i forget the wp global objects are all lowercase

--- ipp/archive-publication.php
+++ ipp/archive-publication.php
@@ -2,7 +2,7 @@
 //@todo move up higher
 require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'models'.DIRECTORY_SEPARATOR.'base.php';
 
-global $WP_Query;
+global $wp_query;
 $objWpBase = new WpBase();
 
 var_export($WP_Query);
