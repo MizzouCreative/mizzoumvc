4ca850ddf7b902b6b3e86291e337e041692e8c78
Paul Gilzow <gilzowp@missouri.edu>
2014-05-23 13:49:31 -0500

    restore view output

--- ipp/archive-publication.php
+++ ipp/archive-publication.php
@@ -10,7 +10,8 @@ $objWpBase = new WpBase();
 $aryData['objMainPost'] = new MizzouPost($post);
 $aryData['aryPublications'] = $objWpBase->convertPosts($wp_query->posts);
 
+/*
 echo '<xmp>',var_export($wp_query,true),'</xmp>',PHP_EOL,PHP_EOL;
 echo '<xmp>',var_export($aryData,true),'</xmp>';
-
-//mizzouOutPutView('archive-publication',$aryData);
+*/
+mizzouOutPutView('archive-publication',$aryData);
