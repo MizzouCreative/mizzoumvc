effebc4a1dd3eee461d42ce993ab34a26b19e67c
Paul Gilzow <gilzowp@missouri.edu>
2014-05-29 17:11:21 -0500

    remove logger from date formatter method

--- a11y-base/class-MizzouPost.php
+++ a11y-base/class-MizzouPost.php
@@ -98,7 +98,6 @@ class MizzouPost
 
     private function _setFormattedDate()
     {
-        _mizzou_log($this->date,'trying to set a format on a date using format: ' . $this->aryOptions['date_format']);
         $this->formatted_date = date($this->aryOptions['date_format'],strtotime($this->date));
     }
 } 
