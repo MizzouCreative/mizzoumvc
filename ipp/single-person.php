a2a5c5f5cb444703dfe6d089fe18449d26674834
Paul Gilzow <gilzowp@missouri.edu>
2014-05-29 15:58:54 -0500

    retrieve related publications

--- ipp/single-person.php
+++ ipp/single-person.php
@@ -12,17 +12,21 @@
  */
 
 /**
- * get our model
+ * get our models
  * @todo we should be able to make this a function and move up higher
  */
 require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'models'.DIRECTORY_SEPARATOR.'people.php';
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'models'.DIRECTORY_SEPARATOR.'publication.php';
 //@todo move this up higher as well
 require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'models'.DIRECTORY_SEPARATOR.'viewOutput.php';
 
 $aryData = array();
 
 $objStaffModel = new People();
+$objPublicationModel = new Publication();
+
 $objPerson = $objStaffModel->retrieveStaff($post);
+$aryPublications = $objPublicationModel->getPublicationsByStaff($objPerson->ID);
 
 $aryData['objPerson'] = $objPerson;
 
@@ -31,4 +35,9 @@ $aryData['objPerson'] = $objPerson;
 
 <xmp>
     <?php var_export($objPerson); ?>
+</xmp>
+
+<p>Related Publications:</p>
+<xmp>
+    <?php var_export($aryPublications); ?>
 </xmp>
