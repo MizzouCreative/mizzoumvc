cb64bb1a69742f78272cec8c9ee15bba23408c3e
Paul Gilzow <gilzowp@missouri.edu>
2014-06-02 15:52:45 -0500

    uh...

--- ipp/archive-project.php
+++ ipp/archive-project.php
@@ -22,10 +22,7 @@ global $wp_query;
 $aryData = array();
 $objWpBase = new WpBase();
 
-//$aryData['objMainPost'] = new MizzouPost($post);
 $aryProjects = $objWpBase->convertPosts($wp_query->posts);
-
-_mizzou_log($aryData['aryProjects'],'our converted projects');
 
 //we need to get the contents from the loop view
 ob_start();
@@ -33,8 +30,6 @@ require_once 'views' . DIRECTORY_SEPARATOR . 'projects-loop.php';
 $aryData['strLoopContent'] = ob_get_contents();
 
 ob_end_clean();
-
-_mizzou_log($aryData['strLoopContent'],'contents of LoopContent');
 
 $aryData['strPageTitle'] = post_type_archive_title('',false);
 mizzouOutPutView('archive-project',$aryData);
