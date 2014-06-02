6159826b3f174075343895cfc7ae07139f641f71
Paul Gilzow <gilzowp@missouri.edu>
2014-06-02 15:58:27 -0500

    documentation

--- ipp/views/archive-project.php
+++ ipp/views/archive-project.php
@@ -1,5 +1,21 @@
 <?php
 /**
+ * Inner view file for Project archive page
+ *
+ *
+ * To facilitate this, you have access to the following variables:
+ *  - $strLoopContent
+ *
+ * You do not need to call get_header(), get_footer, get_sidebar() or breadcrumbs() as those are handled by outer
+ * functions.
+ *
+ * @package WordPress
+ * @subpackage IPP
+ * @category theme
+ * @category view
+ * @author Paul Gilzow, Web Communications, University of Missouri
+ * @copyright 2014 Curators of the University of Missouri
+ *
  * $strLoopContent comes from views/projects-loop
  */
 ?>
