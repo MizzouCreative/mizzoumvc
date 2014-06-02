d9488ce114b7270112ab43694196f72d83188a38
Paul Gilzow <gilzowp@missouri.edu>
2014-06-02 15:29:42 -0500

    documentation

--- ipp/views/page.php
+++ ipp/views/page.php
@@ -1,9 +1,21 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Frank
- * Date: 5/30/14
- * Time: 2:58 PM
+ * Inner view file for general pages
+ *
+ *
+ * Has access to the following variables:
+ *  - $objMainPost Mizzou Post object for the page
+ *
+ * You do not need to call get_header(), get_footer, get_sidebar() or breadcrumbs() as those are handled by outer
+ * functions.
+ *
+ * @package WordPress
+ * @subpackage IPP
+ * @category theme
+ * @category view
+ * @author Paul Gilzow, Web Communications, University of Missouri
+ * @copyright 2014 Curators of the University of Missouri
+ *
  */
 ?>
 <section aria-label="content" role="region">
