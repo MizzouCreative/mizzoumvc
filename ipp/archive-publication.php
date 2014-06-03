8bf6df367dedf6d4f812a33d86dda394408f5563
Paul Gilzow <gilzowp@missouri.edu>
2014-06-03 16:52:37 -0500

    try again

--- ipp/archive-publication.php
+++ ipp/archive-publication.php
@@ -23,6 +23,6 @@ $aryData['aryPublicationsGroup'] = $objWpBase->convertPosts($wp_query->posts,$ar
 /*
 echo '<xmp>',var_export($wp_query,true),'</xmp>',PHP_EOL,PHP_EOL;*/
 
-echo '<xmp>',var_export($aryData['aryPublicationsGroup'],true),'</xmp>';
+//echo '<xmp>',var_export($aryData['aryPublicationsGroup'],true),'</xmp>';
 
-//mizzouOutPutView('archive-publication',$aryData);
+mizzouOutPutView('archive-publication',$aryData);
