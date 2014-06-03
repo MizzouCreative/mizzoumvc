840f98e3377d36c8e24272d10494cac5053e7cba
Paul Gilzow <gilzowp@missouri.edu>
2014-06-03 13:59:16 -0500

    small refactoring

--- ipp/views/blog.php
+++ ipp/views/blog.php
@@ -4,25 +4,31 @@
  * User: gilzow
  * Date: 6/3/14
  * Time: 12:13 PM
+ * @todo we should be able to take the hard-coded value (2) at line 34 and move it into a config/theme option area. Then
+ * define two column classes: first (alpha) and last (omega).  Set the default column class to empty. If the counter is
+ * on 0, reset the column class to first. If we are one less than the number of desired columns (e.g. desired columns is
+ * 3, so when the counter hits 2), reset the column class to last.
  */
 
-/**
- * If we are on the first colum (key==0), the class should be alpha, nothing if we are on the second, and omega if we
- * are on the third column
- */
-$aryColClasses = array(
-    0=>'alpha',
-    1=>'',
-    2=>'omega'
-);
+$strFirstColumnClass = 'alpha';
+$strLastColumnClass = 'omega';
+$intDesiredColumns = 4;
+$intLastColumn = $intDesiredColumns - 1;
 ?>
 
 <section aria-label="content" role="region">
     <?php if(count($aryPosts) > 0) : $intCounter = 0; ?>
-        <?php foreach($aryPosts as $objPost) : ?>
-            <div class="span3 <?php echo $aryColClasses[$intCounter]; ?>">
+        <?php foreach($aryPosts as $objPost) : $strColumnClass = ''; ?>
+            <?php
+                if($intCounter == 0) {
+                    $strColumnClass = $strFirstColumnClass;
+                } elseif($intCounter == $intLastColumn) {
+                    $strColumnClass = $strLastColumnClass;
+                }
+            ?>
+            <div class="span3 <?php echo $strColumnClass; ?>">
                 <div class="post-item">
-                    <a class="clearfix post-linkl" href="<?php $objPost->permalink; ?>">
+                    <a class="clearfix post-linkl" href="<?php echo $objPost->permalink; ?>">
                         <h3 class="post-title"><?php echo $objPost->title; ?></h3>
                     </a>
                 </div>
@@ -30,7 +36,7 @@ $aryColClasses = array(
             <!--
             <?php var_export($objPost);?>
             -->
-            <?php if(2 == $intCounter ) : $intCounter = 0;?>
+            <?php if($intCounter == $intLastColumn ) : $intCounter = 0;?>
                 <div class="clear"></div>
             <?php else : ++$intCounter; ?>
             <?php endif; //endif could be on the line above, but this makes it more readable ?>
