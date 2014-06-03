3c6abc42fb7e2f19e51f2b8140034327214763c7
Paul Gilzow <gilzowp@missouri.edu>
2014-06-03 13:33:44 -0500

    call view

--- ipp/home.php
+++ ipp/home.php
@@ -1,6 +1,18 @@
 <?php
 /**
- * Home page controller.
+ * Blogs archive controller.
+ * @see http://codex.wordpress.org/Creating_a_Static_Front_Page#Custom_Blog_Posts_Index_Page_Template
  */
-?>
-<p>home page controller</p>
+
+//@todo move this up higher as well
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'models'.DIRECTORY_SEPARATOR.'viewOutput.php';
+//@todo move this up higher as well
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'models'.DIRECTORY_SEPARATOR.'base.php';
+
+global $wp_query;
+
+$aryData = array();
+$objWpBase = new WpBase('post_');
+$aryData['aryPosts'] = $objWpBase->convertPosts($wp_query->posts,array('include_meta'=>true));
+
+mizzouOutPutView('blog',$aryData);
