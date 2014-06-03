8966e255cb6e945050113ac750aa5a71e5fe4a3f
Paul Gilzow <gilzowp@missouri.edu>
2014-06-03 09:46:02 -0500

    add explicit support for booleans

--- a11y-base/functions.php
+++ a11y-base/functions.php
@@ -298,7 +298,7 @@ if(!function_exists('_mizzou_log')){
       if( is_array( $mxdVariable ) || is_object( $mxdVariable ) ){
          $strMessage .= PHP_EOL . var_export($mxdVariable,true);
       } elseif(is_bool($mxdVariable)) {
-        $strMessage .= 'Boolean: ' . ($mxdVariable) ? 'true' : 'false';
+        $strMessage .= 'Boolean: ' . (true === $mxdVariable) ? 'true' : 'false';
       } else {
           $strMessage .= $mxdVariable;
       }
