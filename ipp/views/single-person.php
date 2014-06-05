53a3b74eb2432e84d8a9f550e10cca262be55c0b
Paul Gilzow <gilzowp@missouri.edu>
2014-06-05 15:11:32 -0500

    use publication-loop.php

--- ipp/views/single-person.php
+++ ipp/views/single-person.php
@@ -152,26 +152,7 @@ global $objPerson;
     <?php if(count($aryPublications) > 0) : ?>
     <h2>Selected Publications</h2>
     <div class="clearfix">
-        <?php foreach($aryPublications as $objPublication) : ?>
-            <div class="publication-item">
-                <h4>
-                    <a title="<?php echo $objPublication->title; ?>" rel="bookmark" href="<?php echo $objPublication->permalink; ?>"><?php echo $objPublication->title; ?></a>
-                    <?php if('' != $objPublication->authors) : ?>
-                    <p><?php echo $objPublication->authors; ?></p>
-                    <?php endif; ?>
-                    <p><?php echo $objPublication->formatted_date; ?></p>
-                    <?php if('' != $objPublication->content) : ?>
-                    <?php
-                        /**
-                         * 20140530 PFG:
-                         * Do we want content_raw here? using content brings in the formatted version with <p> included.
-                         * Removed the <p></p> surrounding content for now
-                         */
-                        echo $objPublication->content; ?>
-                    <?php endif; ?>
-                </h4>
-            </div>
-        <?php endforeach; ?>
+        <?php require 'publication-loop.php'; ?>
     </div>
     <p>
         <a href="<?php echo $strPublicationArchiveURL ?>">Complete publications list</a>
