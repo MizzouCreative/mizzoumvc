8647362a774f773a735b051d347225e579ff8cac
Paul Gilzow <gilzowp@missouri.edu>
2014-06-05 15:13:51 -0500

    forgot your directory separator

--- ipp/taxonomy-author_archive.php
+++ ipp/taxonomy-author_archive.php
@@ -12,4 +12,4 @@
  * @author Paul Gilzow, Web Communications, University of Missouri
  * @copyright 2014 Curators of the University of Missouri
  */
-require_once(basename(__FILE__).'archive-publication.php');
+require_once(basename(__FILE__).DIRECTORY_SEPARATOR.'archive-publication.php');
