1d9be5c529b125704c98b9af8558d20ae93cddcb
Paul Gilzow <gilzowp@missouri.edu>
2014-06-09 15:17:42 -0500

    typo in aryOption key

--- ipp/single-publication.php
+++ ipp/single-publication.php
@@ -12,5 +12,5 @@ require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'models'.DIRECTORY_SEPARA
 $objPublicationModel = new Publication();
 $aryData = array();
 
-$aryData['objMainPost'] = $objPublicationModel->convertPosts(array($post),array('include_neta'=>true, 'format_date'=>true,'date_format'=>'F Y'));
+$aryData['objMainPost'] = $objPublicationModel->convertPosts(array($post),array('include_meta'=>true, 'format_date'=>true,'date_format'=>'F Y'));
 mizzouOutPutView('single-publication',$aryData);
