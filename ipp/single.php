c06593d0396bb6798673c997222123fc5b71ae64
Paul Gilzow <gilzowp@missouri.edu>
2014-06-09 14:51:16 -0500

    add related posts, also default

--- ipp/single.php
+++ ipp/single.php
@@ -6,6 +6,18 @@
  * Time: 7:41 AM
  */
 
+//@todo move this up higher as well
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'models'.DIRECTORY_SEPARATOR.'viewOutput.php';
+/**
+ * get our models
+ * @todo we should be able to make this a function and move up higher
+ * @todo change this to the specific model once built
+ */
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'models'.DIRECTORY_SEPARATOR.'WpBase.php';
+
+/**
+ * @todo move this into the model
+ */
 $aryOptions = array(
     'include_meta'=>true,
     'format_date'=>true,
@@ -13,4 +25,27 @@ $aryOptions = array(
 );
 
 $objMainPost = new MizzouPost($post,$aryOptions);
-var_export($objMainPost);
+
+/**
+ * 20140609 PFG: @wp-hack.  Ability to retrieve associated taxonomies is partially implemented, but not complete. Fake it
+ * until after the 20140610 meeting. Definitely needs to be moved into a
+ */
+$aryCategories = wp_get_post_categories($post->ID);
+if (count($aryCategories) > 0) {
+    //Assume that we always want the first category
+    $aryArgs = array(
+        'cat' => $aryCategories[0],
+        'post__not_in'=> array($objMainPost->ID),
+        'posts_per_page'=>5,
+        'ignore_sticky_posts'=>true
+    );
+
+    $aryRelatedPosts = new WP_Query($aryArgs);
+    $objPostModel = new WpBase();
+    $aryData['aryRelatedPosts'] = $objPostModel->convertPosts($aryRelatedPosts);
+} else {
+    $aryData['aryRelatedPosts'] = array();
+}
+
+mizzouOutPutView('single',$aryData);
+//var_export($objMainPost);
