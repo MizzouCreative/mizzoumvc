d6f9fcca3ec84e491efe50e1717619a55e4d8d80
Paul Gilzow <gilzowp@missouri.edu>
2014-06-09 08:28:57 -0500

    intial commit

---
+++ ipp/single.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gilzow
+ * Date: 6/9/14
+ * Time: 7:41 AM
+ */
+
+$objMainPost = new MizzouPost($post,array('include_meta'=>true));
+var_export($objMainPost);
