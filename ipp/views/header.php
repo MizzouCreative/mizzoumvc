d344749b4feb4eaada7d121b44c7baebde7a0d23
Paul Gilzow <gilzowp@missouri.edu>
2014-06-16 14:14:45 -0500

    intial commit

---
+++ ipp/views/header.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gilzow
+ * Date: 6/16/14
+ * Time: 2:09 PM
+ *
+ * Will need
+ *  - objMainPost w/ custom meta data
+ *  - strPageTitle
+ *  - template location
+ *  - site option for tracking code
+ *  - blog url
+ *  - blog name
+ *  - navigation menus: audience and primary
+ *  - search form
+ *  - contents of wp_head
+ */ 
