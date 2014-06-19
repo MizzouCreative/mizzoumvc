64143911d153335284e0134cd25af0aca37b16af
Paul Gilzow <gilzowp@missouri.edu>
2014-06-19 14:50:05 -0500

    change variables to point to objSite, documentation

--- ipp/views/footer.php
+++ ipp/views/footer.php
@@ -2,15 +2,29 @@
 /**
  * View file used to render the last section of every page
  *
- * Has access to the following variables
- *  - $strSiteURL - Site's root URL
- *  - $strSiteName - Name of the site
- *  - $strPageList - html formatted list of pages in wordpress as returned by wp_list_pages
- *  - $strParentThemeURL - Parent Theme's root URL
- *  - $strChildThemeURL - Child Theme's root URL
- *  - $strModifiedDate - last modified date of either the page/post/CPT or the last update made to the site in general
- *  - $intCopyrightYear - year to be used for copyright purposes
- *  - $strWpFooterContents - contents as returned by wp_footer()
+ * Needs the following data
+ *  - Site's root URL: see $objSite below
+ *  - Name of the site: see $objSite below
+ *  - html formatted list of pages in wordpress as returned by wp_list_pages: see $objSite below
+ *  - Parent Theme's root URL: see $objSite below
+ *  - Child Theme's root URL: see $objSite below
+ *  - Last modified date of either the page/post/CPT or the last update made to the site in general: see $objSite below
+ *  - Current copyright year: see $objSite below
+ *  - Contents as returned by wp_footer(): $strWpFooterContents
+ *
+ *  $objSite contains
+ *  -> CopyRightYear (also accessible as $strCopyRightYear)
+ *  -> Name (also accessible as $strSiteName)
+ *  -> URL (also accessible as $strSiteURL)
+ *  -> ParentThemeURL (also accessible as $strParentThemeURL)
+ *  -> ChildThemeURL (also accessible as $strChildTheme)
+ *  -> ActiveStylesheet (also accessible as $strActiveStylesheet)
+ *  -> ActiveThemeURL (also accessible as $strActiveThemeURL)
+ *  -> TrackingCode (also accessible as $strTrackingCode)
+ *  -> PrimaryMenu (also accessible as $strPrimaryMenu)
+ *  -> AudienceMenu (also accessible as $strAudienceMenu)
+ *  -> LastModifiedDate (also accessible as $strLastModifiedDate)
+ *  -> PageList (also accessible as $strPageList)
  *
  * @package WordPress
  * @subpackage IPP
@@ -39,8 +53,8 @@
 
 
 <ol class="mobilenav menu">
-    <li class="home"><a href="<?php echo $strSiteURL; ?>"><?php echo $strSiteName; ?></a></li>
-    <?php echo $strPageList; ?>
+    <li class="home"><a href="<?php echo $objSite->URL; // $strSiteURL; ?>"><?php echo $objSite->Name; // $strSiteName; ?></a></li>
+    <?php echo $objSite->PageList; //$strPageList; ?>
 </ol>
 
 </nav>
@@ -58,7 +72,7 @@
             <a href="http://missouri.edu/" title="University of Missouri home">
                 <span class="shield">
                     <svg width="86px" height="50px">
-                        <image xlink:href="<?php echo $strChildThemeURL; ?>/images/mu-mark.svg" alt="MU Logo" src="<?php echo $strChildThemeURL; ?>/images/mu-mark.png" width="86px" height="50px"/>
+                        <image xlink:href="<?php echo $objSite->ChildThemeURL; //$strChildThemeURL; ?>/images/mu-mark.svg" alt="MU Logo" src="<?php echo $objSite->ChildThemeURL;// $strChildThemeURL; ?>/images/mu-mark.png" width="86px" height="50px"/>
                     </svg>
                 </span>
                 <span class="text">
@@ -82,7 +96,7 @@
         <a class="alert-icon" href="http://mualert.missouri.edu">
             <span class="exclaim">
                 <svg width="24" height="24">
-                    <image xlink:href="<?php echo $strParentThemeURL; ?>/images/exclaim.svg" alt="" src="<?php echo $strParentThemeURL; ?>/images/exclaim.png" width="24" height="24"/>
+                    <image xlink:href="<?php echo $objSite->ParentThemeURL; // $strParentThemeURL; ?>/images/exclaim.svg" alt="" src="<?php echo $objSite->ParentThemeURL; //$strParentThemeURL; ?>/images/exclaim.png" width="24" height="24"/>
                 </svg>
             </span>
             Emergency Information
@@ -95,7 +109,7 @@
         </div>  <!-- accessibility -->
         <div class="accessibility">
             <!-- removed temporarily 20140610 PFG
-                            <a href="<?php echo $strSiteURL; ?>/a-z/">A-Z Index</a>
+                            <a href="<?php echo $objSite->URL; // $strSiteURL; ?>/a-z/">A-Z Index</a>
                             -->
         </div>  <!-- accessibility -->
     </div> <!-- span2 -->
@@ -104,11 +118,11 @@
     <div class="clear"></div>
 
     <div class="legal span12">
-        Copyright &#169; <time datetime="<?php echo $intCopyrightYear; ?>"><?php echo $intCopyrightYear; ?></time> &#8212; Curators of
+        Copyright &#169; <time datetime="<?php echo $objSite->CopyrightYear; // $intCopyrightYear; ?>"><?php echo $objSite->CopyrightYear; //$intCopyrightYear; ?></time> &#8212; Curators of
         the University of Missouri. All rights reserved. <a href="http://www.missouri.edu/dmca/">DMCA</a> and
         <a href="http://missouri.edu/copyright/">other copyright information</a>. An
         <a href="http://missouri.edu/eeo-aa/">equal opportunity/affirmative action</a> institution. Published by
-        <a href="<?php echo $strSiteURL; ?>"><?php echo $strSiteName; ?></a>. Updated: <?php echo $strModifiedDate; ?>
+        <a href="<?php echo $objSite->URL; // $strSiteURL; ?>"><?php echo $objSite->Name; //$strSiteName; ?></a>. Updated: <?php echo $objSite->LastModifiedDate;// $strModifiedDate; ?>
     </div> <!--  span12 -->
 
 
