e79ee7e6b8f1f46948acab67bcf11baffa10e9e3
Paul Gilzow <gilzowp@missouri.edu>
2014-06-23 14:20:54 -0500

    initial commit

---
+++ ipp/models/slide.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gilzow
+ * Date: 6/23/14
+ * Time: 1:33 PM
+ */
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'WpBase.php';
+
+class Slide extends WpBase {
+    protected $strPostType = 'project';
+} 
