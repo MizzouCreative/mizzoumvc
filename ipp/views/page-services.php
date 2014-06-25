069389067f30bd605bc70037badf7f53ce05f5f8
Paul Gilzow <gilzowp@missouri.edu>
2014-06-25 15:01:40 -0500

    documentation

--- ipp/views/page-services.php
+++ ipp/views/page-services.php
@@ -10,6 +10,9 @@
  * You do not need to call get_header(), get_footer, get_sidebar() or breadcrumbs() as those are handled by outer
  * functions.
  *
+ * **NOTE** 20140624 PFG: had no idea what you wanted in terms of mark up.  I did speak to Gen/Lisa and they suggested
+ * the area be named "Project Spotlight"
+ *
  * @package WordPress
  * @subpackage IPP
  * @category theme
@@ -24,7 +27,7 @@
     <?php if(isset($objFeaturedPost) && is_object($objFeaturedPost)) : ?>
         <div class="clearfix"></div>
         <div>
-            <h3>Featured Post? Or do we call this 'Featured Project'?</h3>
+            <h3>Project Spotlight</h3>
             <a href="<?php echo $objFeaturedPost->permalink; ?>"><?php echo $objFeaturedPost->title; ?></a>
         </div>
     <?php endif; ?>
