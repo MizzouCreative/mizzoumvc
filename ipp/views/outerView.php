a69db118537bb408aa78dd37c58846bb95f135f0
Paul Gilzow <gilzowp@missouri.edu>
2014-06-26 17:09:03 -0500

    add support for featured image above the H1, and class added to H1

--- ipp/views/outerView.php
+++ ipp/views/outerView.php
@@ -21,6 +21,10 @@
 
 $strPreBreadcrumbDivContents = ($boolIncludeSidebar) ? 'flex span8' : 'span12';
 /**
+ * Does the H1 need a class?
+ */
+$boolH1Class = false;
+/**
  * Does the page contain a sidebar? If so, what class(es) should the content well contain? Otherwise, what class should
  * it contain?
  *
@@ -36,8 +40,13 @@ $strPreBreadcrumbDivContents = ($boolIncludeSidebar) ? 'flex span8' : 'span12';
     <main id="main" role="main">
         <div id="content">
             <article role="article">
+                <?php if($objMainPost->post_type == 'page' && $objMainPost->image != '') : $strH1Class = 'featured'; ?>
+                    <div class="featured-image-wrapper">
+                        <img src="<?php echo $objMainPost->image->src_full; ?>" alt="<?php $objMainPost->image->alt; ?>" />
+                    </div>
+                <?php endif;?>
                 <header>
-                    <h1 id="title"><?php echo $strPageTitle; if($strEditPostLink != '') :?> <a href="<?php echo $strEditPostLink; ?>" class="post-edit-link">Edit</a><?php endif; ?></h1>
+                    <h1 id="title"<?php if($boolH1Class): ?> class="featured"<?php endif; ?>><?php echo $strPageTitle; if($strEditPostLink != '') :?> <a href="<?php echo $strEditPostLink; ?>" class="post-edit-link">Edit</a><?php endif; ?></h1>
                 </header>
                 <?php echo $strInnerViewContent;?>
             </article>
