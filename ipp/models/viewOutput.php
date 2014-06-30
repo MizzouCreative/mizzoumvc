7bff82da94c7ab5c468441dc8f0115f1a9b77a47
Paul Gilzow <gilzowp@missouri.edu>
2014-06-30 15:35:02 -0500

    moved logic to determine whether or not include featured image above the H1 on a page

--- ipp/models/viewOutput.php
+++ ipp/models/viewOutput.php
@@ -34,8 +34,9 @@ function mizzouOutPutView($strInnerViewFileName,$aryData,$aryOptions=array())
         'graduate-research-assistants'
     );
 
-    $boolIncludeNoIndex = false;
-    $boolIncludeSidebar = false;
+    $boolIncludeNoIndex             = false;
+    $boolIncludeSidebar             = false;
+    $boolIncludeImageAboveHeader    = false;
 
     //convert all the data for the inner view into variables
     extract($aryData);
@@ -78,8 +79,12 @@ function mizzouOutPutView($strInnerViewFileName,$aryData,$aryOptions=array())
         $boolIncludeSidebar = true;
     }
 
+    if($objMainPost->post_type == 'page' && $objMainPost->image != ''){
+        $boolIncludeImageAboveHeader = true;
+    }
+
     /**
-     * This one is a big of a bugger...
+     * This one is a bit of a bugger...
      * If we have access to the MainPost object AND either noindex or nolink is set and ON
      * OR
      * we're on a 404 page
