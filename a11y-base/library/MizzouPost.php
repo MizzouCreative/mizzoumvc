26bc0f22ff1cd3b153ea1f21eb88c7355d711826
Paul Gilzow <gilzowp@missouri.edu>
2014-07-03 12:32:57 -0500

    debug custom field grep

--- a11y-base/library/MizzouPost.php
+++ a11y-base/library/MizzouPost.php
@@ -114,8 +114,10 @@ class MizzouPost extends PostBase
     {
         //we need the full pattern to use including the prefix, if applicable
         $strFullPattern = $this->_buildFullMetaGroupPattern($aryOptions['meta_prefix']);
+        _mizzou_log($strFullPattern,'the pattern I\'ll use to grep with',array('func'=>__FUNCTION__));
         //find all of the field keys that match our pattern
         $aryMetaGroupKeys = preg_grep($strFullPattern,array_keys($this->aryOriginalCustomData));
+        _mizzou_log($aryMetaGroupKeys,'matches i found from the grep');
         //loop through each match, pull out the group component and add it the group array
         foreach($aryMetaGroupKeys as $strKeyInGroup){
             if(1 === preg_match($strFullPattern,$strKeyInGroup,$aryMatch)){
