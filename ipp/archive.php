43c806dbd69495fea6ea333d289d2a5897fdab49
Paul Gilzow <gilzowp@missouri.edu>
2014-07-08 12:59:03 -0500

    initial commit

---
+++ ipp/archive.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * get our model
+ * @todo we should be able to make this a function and move up higher
+ */
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'models'.DIRECTORY_SEPARATOR.'project.php';
+//@todo move this up higher as well
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'models'.DIRECTORY_SEPARATOR.'viewOutput.php';
+
+global $wp_query;
+$aryData = array();
+/**
+ * Why are we using the base class instead of the project class here?
+ * @todo convert to Project
+ */
+$objWpBase = new WpBase();
+
+$aryData['aryPosts'] = $objWpBase->convertPosts($wp_query->posts);
