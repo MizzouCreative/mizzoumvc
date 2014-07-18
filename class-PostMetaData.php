4dc018a4bd63e9a68e1955b5548bbc991c3a9db7
Paul Gilzow <gilzowp@missouri.edu>
2014-07-18 10:52:40 -0500

    TL;DR - moved all files and directories to the root
    in order to use this repo as a submodule in other "child" repos, it needs to be in a dedicated directory at the root of the calling repo. In order then for wordpress to see it as a theme, it will need to be a sibling directory of the child theme contained in the calling repo.

---
+++ class-PostMetaData.php
@@ -0,0 +1,329 @@
+<?php
+/**
+* Gathers all the custom data related to a post
+* 
+* @package WordPress
+* @subpackage theme-helper
+* @category theme
+* @category class
+* @author Paul F. Gilzow & Jason Rollins, Web Communications, University of Missouri
+* @copyright 2013 Curators of the University of Missouri
+* @version 201303281212
+* 
+* 
+*/
+require_once(dirname(__FILE__) . '/class-CustomPostTypeAbstract.php');
+require_once(dirname(__FILE__) . '/ImageData.php');
+
+class PostMetaData extends CustomPostType{
+    /**
+    * All of our custom fields are prefixed by their custom post type type (ie news, people, etc).  When dealing with properties from this 
+    * object, we want to access them directly (ie first_name instead of people_first_name).
+    * 
+    * @var string
+    */
+    private $strPrefix = null;
+    
+    /**
+    * put your comment there...
+    * 
+    * @var object
+    */
+    public $featured_image = null;
+    
+    /**
+    * stores data on the images that might be attached to the post
+    * 
+    * @var array
+    */
+    private $image_data = null;            
+
+
+    private $strMetaGroupPattern = '([a-zA-Z]*)\d';
+
+    private $strFullMetaGroupPattern = '';
+    /**
+    * put your comment there...
+    * 
+    * @param integer $intPostID
+    * @param string $strPrefix
+    * @param boolean $boolSuppressEmptyFieldInGroup
+    * @return PostMetaData
+    */
+    function __construct($intPostID,$strPrefix = null,$boolSuppressEmptyFieldInGroup = false)
+    {
+        if(is_numeric($intPostID)){
+            $this->intPostID = $intPostID; 
+            if(!is_null($strPrefix)) $this->strPrefix = $strPrefix; 
+            $this->_retrieve_wp_data();
+            $this->_reformat_data();
+            $this->_consolidateMetaGroups($boolSuppressEmptyFieldInGroup);
+            $this->_retrieve_permalink();
+            $this->_retrieve_post_format();
+        } else {
+            $this->add_error('Post ID given in constructor must be numeric');
+        }
+    }
+    
+    /**
+    * Retrieves the custom post data for a post from wordress
+    * 
+    */
+    protected function _retrieve_wp_data()
+    {
+        $this->aryOriginalData = get_post_custom($this->intPostID);        
+    }
+    
+    protected function _retrieve_permalink()
+    {
+        $this->add_data('permalink', get_permalink($this->intPostID));
+    }
+    
+    protected function _retrieve_post_format()
+    {
+        $this->add_data('post_format', (FALSE !== $strPostFormat = get_post_format($this->intPostID)) ? $strPostFormat : 'standard');
+    }
+
+    
+    /**
+    * Reformats the post's custom data.
+    * 
+    * Removes the prefix from the id, and removes any internal custom data fields
+    * 
+    */
+    private function _reformat_data()
+    {
+        $boolRemovePrefix = (!is_null($this->strPrefix)) ? true : false;
+        if($boolRemovePrefix) $intPrefixLen = strlen($this->strPrefix);
+        foreach($this->aryOriginalData as $key=>$val){
+            if(strpos($key,'_') !== 0){ //we dont need the interal custom data keys=>vals
+                if($boolRemovePrefix){
+                    $key = substr($key,$intPrefixLen);
+                }
+                
+                $this->aryData[$key] = $val[0];    
+            }    
+        }
+    } 
+    
+    /**
+    * put your comment there...
+    * 
+    * @param array $aryItems
+    * @param string $strValue
+    */
+    public function includeSideBar(array $aryItems,$strValue)
+    {
+        $boolIncludeSidebar = false;
+        
+        if($this->member_of_group_set($aryItems)){
+            //we know that at least one of the items we need ffrom the groups is set.
+            $intNumItems = count($aryItems);
+            $i = 0;
+            while($i<$intNumItems && !$boolIncludeSidebar){
+                if($this->get($aryItems[$i]) == $strValue){
+                    $boolIncludeSidebar = true;
+                }
+                ++$i;
+            }     
+        } 
+        
+        return $boolIncludeSidebar;    
+    }
+    
+    /**
+    * put your comment there...
+    * 
+    * @param array $aryOptions
+    * @return mixed
+    */
+    public function retrieve_feature_image_data($aryOptions=array())
+    {
+        $aryDefaults = array(
+            'return'=>true,
+            'captions'=>false
+        );
+        
+        $aryOptions = $this->parse_arguments($aryOptions,$aryDefaults);
+        
+        if(!isset($this->featured_image)){
+            if(!$this->image_data_set()) $this->image_data = array();
+            if(has_post_thumbnail($this->intPostID)){
+                $this->add_data('featured_image',get_post_thumbnail_id($this->intPostID));
+                //$this->featured_image = ;
+                $this->image_data[$this->aryData['featured_image']] = new ImageData($this->aryData['featured_image'],$aryOptions['captions']);                  
+            } else {
+                $this->add_data('featured_image', FALSE);
+            }
+  
+        }
+
+        if($aryOptions['return'] ){
+            return (is_bool($this->aryData['featured_image'])) ? $this->aryData['featured_image'] : $this->image_data[$this->aryData['featured_image']];
+        }
+            
+    }
+    
+    /**
+    * put your comment there...
+    * 
+    * @param array $aryOptions
+    * @param array $aryDefaults
+    * @return array
+    * @uses wp_parse_args() from wordpress core. Could be replaced with a standard array_merge if needed
+    */
+    protected function parse_arguments($aryOptions,$aryDefaults)
+    {
+//         catch legacy code and inproper argument
+         if(is_bool($aryOptions)){
+            $aryOptions = array('return'=>$aryOptions);
+            $this->_log($aryOptions,'legacy code use detected.',true,array('func'=>__FUNCTION__,'line'=>__LINE__));    
+         } elseif(!is_array($aryOptions)){
+             $mxdOptions = array();
+             $this->_log(null,'Argument needs to be an an array',false,array('func'=>__FUNCTION__,'line'=>__LINE__));
+         }
+         
+         return wp_parse_args($aryOptions,$aryDefaults);        
+    }
+    
+    /**
+    * put your comment there...
+    * 
+    * @param array $aryOptions
+    * @return array
+    */
+    public function retrieve_gallery_images($aryOptions=array())
+    {
+         $aryDefaults = array(
+            'return'=>true,
+            'featured'=>true,
+            'captions'=>false
+         );
+         
+         $aryOptions = $this->parse_arguments($aryOptions,$aryDefaults);
+         
+    
+//        do we have any image data at all? Or do we have only featured image data?
+        if(!$this->image_data_set() || (isset($this->aryData['featured_image']) && count($this->image_data) == 1)){
+//           do we need to set the image_data to an array?
+            if(!$this->image_data_set()) $this->image_data = array();
+//            do we have info on the featured image?
+            if(!isset($this->aryData['featured_image'])){
+                $this->retrieve_feature_image_data(false);
+            }
+            
+            $aryParams = array(
+                'numberposts'       => 5,
+                'post_mime_type'    => 'image',
+                'post_type'         => 'attachment',
+                'post_parent'       => $this->intPostID,
+                'orderby'           => 'menu_order ASC, ID', 
+                'order'             => 'DESC'
+            );
+            
+            $aryImages = get_posts($aryParams);
+            //$this->_log($aryImages,'images for post '.$this->intPostID);
+            foreach($aryImages as $objImage){
+                if(!array_key_exists($objImage->ID,$this->image_data)){
+                    $objImageData = new ImageData($objImage->ID,$aryOptions['captions']);
+                    $this->image_data[$objImage->ID] = $objImageData;    
+                }
+            }            
+        }
+        
+        if($aryOptions['return']){
+            if($aryOptions['featured']){
+                return $this->image_data;
+            } else {
+                return array_slice($this->image_data,1,(count($this->image_data)-1),true);
+            }
+                    
+        }
+    }
+    
+    /**
+    * Checks to see if we have image data
+    * 
+    * @return boolean
+    * 
+    */
+    protected function image_data_set()
+    {
+        return is_array($this->image_data);
+    }
+    
+    public function retrieve_parent_name()
+    {
+        if(!$this->is_set('post_parent_name')){
+            $strParent = '';
+            $objPost = get_post($this->intPostID);
+            if(is_integer($objPost->post_parent) && $objPost->post_parent != 0){
+                $objPostParent = get_post($objPost->post_parent);
+                $strParent = $objPostParent->post_name;
+            }           
+        
+            $this->add_data('post_parent_name', $strParent);
+        }
+        
+        return $this->get('post_parent_name');
+    }
+
+    protected function _consolidateMetaGroups($boolSuppressEmptyFields)
+    {
+        _mizzou_log($boolSuppressEmptyFields,'are we suppressing empty fields?',false,array('func'=>__FUNCTION__));
+        //we need the full pattern to use including the prefix, if applicable
+        $strFullPattern = $this->_buildFullMetaGroupPattern();
+        //find all of the field keys that match our pattern
+        //_mizzou_log($strFullPattern,'my full pattern for finding meta groups');
+        //_mizzou_log($this->aryOriginalData,'contents of the original data array');
+        $aryMetaGroupKeys = preg_grep($strFullPattern,array_keys($this->aryOriginalData));
+        //_mizzou_log($aryMetaGroupKeys,'my meta group keys');
+        //loop through each match, pull out the group component and add it the group array
+        foreach($aryMetaGroupKeys as $strKeyInGroup){
+            if(1 === preg_match($strFullPattern,$strKeyInGroup,$aryMatch)){
+                if(!isset($this->aryData[$aryMatch[1]])){
+                    $this->aryData[$aryMatch[1]] = array();
+                } elseif (!is_array($this->aryData[$aryMatch[1]])){
+                    /**
+                     * ok we have a situation where there is already a key set up for the group name, but it isnt an
+                     * array. Most likely we have something like "address" and then "address2". so, let's take the data
+                     * that is currently there and store it, create a new array, and then add that data back in
+                     */
+                    $strLogMsg = "looks like you have a meta field group but the first item is named what the group "
+                        . "name needs to become. Fixing it for you, but you should really go back and rename the key.";
+                    _mizzou_log($aryMatch[1],$strLogMsg,false,array('func'=>__FUNCTION__));
+                    $strTempData = $this->aryData[$aryMatch[1]];
+                    $this->aryData[$aryMatch[1]] = array($strTempData);
+                }
+
+
+                if(!$boolSuppressEmptyFields || ($boolSuppressEmptyFields && trim($this->aryOriginalData[$strKeyInGroup][0]) != '')){
+                    $this->aryData[$aryMatch[1]][] = $this->aryOriginalData[$strKeyInGroup][0];
+                }
+            }
+        }
+    }
+
+    protected function _retrieveMetaGroupKeys()
+    {
+
+    }
+
+    protected function _buildFullMetaGroupPattern()
+    {
+        $strPattern = '';
+        if(!is_null($this->strPrefix)){
+            $strPattern = $this->strPrefix;
+        }
+
+        $strPattern = '/^'.$strPattern.$this->strMetaGroupPattern.'$/';
+
+        return $strPattern;
+    }
+}
+
+/**
+ *CHANGELOG
+ * 20120706 - changed retrieve_feature_image_data() to include a check for whether or not the page has a featured image.  If it doesn't, 'featured_image will return bool false.
+ */
+?>
---
+++ functions.php
@@ -0,0 +1,329 @@
+<?php
+// error_reporting(E_ALL); ini_set('display_errors', 1);
+
+/**
+ * Contains functions necessary for the functionality and display of the theme
+ * 
+ * 
+ * @package WordPress
+ * @subpackage SITENAME
+ * @category theme
+ * @category functions
+ * @author Paul F. Gilzow & Jason L Rollins, Web Communications, University of Missouri
+ * @copyright 2013 Curators of the University of Missouri
+ * @version 201303281326
+ */
+
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'config.php';
+require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'library'.DIRECTORY_SEPARATOR.'Base.php';
+require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'models'.DIRECTORY_SEPARATOR.'Site.php';
+require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'models'.DIRECTORY_SEPARATOR.'Content.php';
+require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'library'.DIRECTORY_SEPARATOR.'MizzouPost.php';
+require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'library'.DIRECTORY_SEPARATOR.'A11yPageWalker.php';
+
+require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'functions'.DIRECTORY_SEPARATOR.'settings.php';
+require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'functions'.DIRECTORY_SEPARATOR.'post-types.php';
+require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'functions'.DIRECTORY_SEPARATOR.'menus.php';
+require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'functions'.DIRECTORY_SEPARATOR.'editor.php';
+require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'functions'.DIRECTORY_SEPARATOR.'childnav.php';
+require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'functions'.DIRECTORY_SEPARATOR.'shortcodes.php';
+require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'functions'.DIRECTORY_SEPARATOR.'widgets.php';
+
+
+function mizzou_setup(){
+    add_filter('query_vars','mizzou_add_URL_query_vars');
+    add_filter('default_hidden_meta_boxes', 'mizzou_display_postexcerpt', 10, 2);
+    add_filter( 'wp_terms_checklist_args', 'mizzou_no_top_float', 10, 2 );
+    add_filter('edit_tag_link', 'edit_tag_link_new_window');
+    // Remove tabindex setting from gravity forms (learned from NewsA11y)
+    add_filter("gform_tabindex", create_function("", "return false;"));
+    /**
+     * Completely disable pingback support
+     * @see http://blog.sucuri.net/2014/03/more-than-162000-wordpress-sites-used-for-distributed-denial-of-service-attack.html
+     */
+    add_filter('xmlrpc_methods', function( $aryMethods ) {
+            unset( $aryMethods['pingback.ping'] );
+            return $aryMethods;
+	}   
+    );
+    
+    /**
+     * Remove link for feeds
+     */
+    remove_action( 'wp_head', 'feed_links', 2);
+    /**
+     * remove link to feeds for extra areas like categories
+     */
+    remove_action('wp_head','feed_links_extra', 3);
+    /**
+     * remove link for windows live writer
+     */
+    remove_action( 'wp_head', 'wlwmanifest_link' );
+    /**
+     * remove generator meta 
+     */
+    remove_action( 'wp_head', 'wp_generator' );
+    /**
+     * remove rsd link
+     */
+    remove_action( 'wp_head', 'rsd_link' ); 
+    /**
+     * remove head links to previous and next
+     */
+    remove_action('wp_head', 'start_post_rel_link', 10); 
+    remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10);
+    
+}
+
+function mizzou_setup_theme(){
+    add_theme_support( 'post-thumbnails' );
+}
+
+/**
+ * Adds the 'q' query var for google search 
+ * 
+ * @param array $aryVars
+ * @return array 
+ */
+function mizzou_add_URL_query_vars($aryVars){
+    $aryVars[] = 'q';
+    return $aryVars;
+}
+
+/**
+ * Takes the 404 URL, strips out the extra characters and ignore words and returns
+ * a string of words to search on
+ * 
+ * @param array $aryIgnoreWords words/characters to ignore, DEFAULT: / - .php .html .aspx
+ * @return string
+ */
+function mizzou_gather_404_search_terms($aryIgnoreWords=null){
+    $aryIgnore = array(
+            '/',
+            '-',
+            '.php',
+            '.html',
+            '.aspx',
+            '.htm',
+            '.cfm',
+            '.asp',
+     );
+    
+    if(!is_null($aryIgnoreWords) && is_array($aryIgnoreWords)){
+        $aryIgnore = array_merge($aryIgnore,$aryIgnoreWords);
+    }
+    
+    $aryReplace = array_fill(0, count($aryIgnore), ' ');
+    
+    $strRequestedPage = $_SERVER['REQUEST_URI'];
+    
+    /**
+    * This would be much better with a regex, but i dont have time right now to 
+    * build it.  We dont care about any url parameters. we need to remove 
+    * index.php and index.html BEFORE we strip out .php and .html since they 
+    * could be looking for a page with the word 'index' that isnt part of 
+    * index.php/html
+    * 
+    * @todo convert this to a regex
+    */    
+    
+    if(FALSE !== $intQMPos = strpos($strRequestedPage, '?')){
+        $strRequestedPage = substr($strRequestedPage, 0,$intQMPos);
+    }
+    
+    $strSearchWords = str_replace(array('index.php','index.html'), array('',''), $strRequestedPage);
+    $strSearchWords = trim(str_replace($aryIgnore, $aryReplace,$strSearchWords));
+    
+    return $strSearchWords;
+    
+}
+
+/**
+ * Restores the Post Excerpt metabox in the write panel for new users
+ * 
+ * Starting at 3.1, Wordpress began hiding the post excerpt by default in the
+ * write panel.  It was still accessible through screen options.  We prefer to
+ * have it displayed by default.  
+ * 
+ * @param array $aryHidden
+ * @param object $objScreen
+ * @return array
+ */
+function mizzou_display_postexcerpt($aryHidden,$objScreen){
+    if($objScreen->base == 'post'){
+        if(FALSE !== $intKey = array_search('postexcerpt', $aryHidden)) unset($aryHidden[$intKey]);
+    }
+    return $aryHidden;
+}
+
+/**
+* Removes dashboard items from the Admin dashboard
+* 
+* @return void
+*/
+function mizzou_remove_dashboard_widgets(){
+    global $wp_meta_boxes;
+    unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_quick_press']);
+    unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_comments']);
+    unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_incoming_links']);
+    unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_plugins']);
+    unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_primary']);
+    unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']);
+    
+}
+
+/**
+ * Disable 'checked_ontop' for taxonomies (learned from NewsA11y)
+ * @param boolean $args
+ * @param type $post_id
+ * @return boolean
+ * @todo complete documentation please
+ */
+function mizzou_no_top_float( $args, $post_id ) {
+  // If the taxonomy is set and equals person_type or group
+	if ( isset( $args['taxonomy'] ) && 'person_type' == $args['taxonomy'] || 'group' == $args['taxonomy'] || 'group' == $args['author_assignment']   )
+		$args['checked_ontop'] = false;
+ 
+	return $args;
+}
+
+
+
+
+/**
+ * Solely here for backwards compatibility and to catch instances when it is still being used
+ */
+class a11y_walker extends A11yPageWalker {
+    public function __construct()
+    {
+        _mizzou_log(self,'old a11y_walker being called. see backtrace',true);
+        parent::__construct();
+    }
+}
+
+/**
+ * If more than one page of archived posts exists, return TRUE.
+ * @global type $wp_query
+ * @return type
+ * @todo documentation
+ */
+function pagination_nav() {
+	global $wp_query;
+	return ($wp_query->max_num_pages > 1);
+}
+
+/**
+ * 
+ * @param type $content
+ * @return type
+ * @todo documentation
+ */
+function edit_tag_link_new_window($content) {
+    $content = preg_replace('/href/', 'target="_blank" href', $content);
+    return $content;
+}
+/**
+ * Last update of anything on the site
+ * @todo documentation
+ * @param boolean $boolReturn, default is false
+ * @global type $wpdb
+ */
+function site_modified_date($boolReturn=false) {
+	global $wpdb;
+	$last_site_update =  $wpdb->get_var( "SELECT post_modified FROM $wpdb->posts WHERE post_status = 'publish' ORDER BY post_modified DESC LIMIT 1" );
+	$strLastUpdate = date('M j, Y', strtotime($last_site_update));
+
+    if($boolReturn){
+        return $strLastUpdate;
+    } else {
+        echo $strLastUpdate;
+    }
+}
+
+/**
+* ================= ACTIONS ================================
+*/
+/**
+*  Removes all of the dashboard widgets except right now and drafts
+*/
+add_action('wp_dashboard_setup','mizzou_remove_dashboard_widgets');
+/**
+* In order to add theme options, they need to be hooked BEFORE the init.
+*/
+add_action('after_setup_theme','mizzou_setup_theme');
+/**
+*  All other filters and actions that dont need to fire before init
+*/
+add_action('init','mizzou_setup');
+
+
+/**
+ * NO ADDITIONAL CODE SHOULD BE BELOW THIS LINE
+* ================= DEBUG =====================================
+*/
+
+if(!function_exists('_mizzou_log')){
+  /**
+  * For logging debug messages into the debug log.
+  * 
+  * To enable logging, you'll need to add the following to @see wp-config.php:
+  * <code>
+        define('WP_DEBUG', true); 
+        define('WP_DEBUG_DISPLAY', false);
+        define('WP_DEBUG_LOG', true);
+  * </code>
+  * 
+   * This will create a debug.log file inside of /wp-content/
+   * 
+  * @param mixed $mxdVariable variable we need to debug
+  * @param $strPrependMessage message to include
+  * @param boolean $boolBackTraced
+  * @param array $aryDetails details for doing a mini backtrace instead of the full thing
+  * 
+  */
+  function _mizzou_log( $mxdVariable, $strPrependMessage = null, $boolBackTraced = false, $aryDetails = array() ) {
+    $boolBackTrace = true;
+    if( WP_DEBUG === true ){
+      $strMessage = 'MIZZOU_LOG: ';
+      
+      if(count($aryDetails) > 0){
+          if(isset($aryDetails['line'])){
+              $strMessage .= 'At line number ' . $aryDetails['line'] . ' ';
+          }
+          
+          if(isset($aryDetails['func'])){
+              $strMessage .= 'inside of function ' . $aryDetails['func'] . ' ';
+          }
+          
+          if(isset($aryDetails['file'])){
+              $strMessage .= 'in file ' . $aryDetails['file'] .' ';
+          }
+          
+          $strMessage .= PHP_EOL;
+      }
+      
+      if(!is_null($strPrependMessage)) $strMessage .= $strPrependMessage.PHP_EOL;
+
+      $strMessage .= 'The variable is a ' . gettype($mxdVariable) . PHP_EOL;
+      
+      if( is_array( $mxdVariable ) || is_object( $mxdVariable ) ){
+         $strMessage .= PHP_EOL . var_export($mxdVariable,true);
+      } elseif(is_bool($mxdVariable)) {
+        $strMessage .= 'Boolean: ';
+        $strMessage .=  (true === $mxdVariable) ? 'true' : 'false';
+      } else {
+          $strMessage .= $mxdVariable;
+      }
+      
+      if($boolBackTrace && $boolBackTraced){
+          $aryBackTrace = debug_backtrace();
+          
+          $strMessage .= PHP_EOL.'Contents of backtrace:'.PHP_EOL.var_export($aryBackTrace,true).PHP_EOL;          
+      }
+
+      $strMessage .= PHP_EOL;
+      error_log($strMessage);
+    }
+  }
+}
+?>
