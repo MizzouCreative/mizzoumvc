a26d7e0c61b63d4d8c53aa6bedc95cdc36c6ed66
Paul Gilzow <gilzowp@missouri.edu>
2014-08-14 11:27:11 -0500

    set $objSite = self;

--- models/Site.php
+++ models/Site.php
@@ -140,7 +140,7 @@ class Site extends Base {
             //.php was not included
             $strViewName .= '.php';
         }
-        $objSite = $this;
+        $objSite = self;
         ob_start();
         require_once locate_template($strViewName,false);
         $strReturn = ob_get_contents();
