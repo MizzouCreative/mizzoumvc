f997df10a4b86eca96623cd0bba15cdccf0e4846
Paul Gilzow <gilzowp@missouri.edu>
2015-01-14 13:57:46 -0600

    initial commit

---
+++ models/Footer.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * 
+ * TL;DR description 
+ *
+ * @package 
+ * @subpackage 
+ * @category 
+ * @category 
+ * @author Paul F. Gilzow, Web Communications, University of Missouri
+ * @copyright 2015 Curators of the University of Missouri
+ */
+
+class Footer extends Subview {
+
+	public function __construct($aryContext)
+	{
+		$this->_setCopyRightYear();
+
+
+	}
+
+	protected function _setCopyRightYear()
+	{
+		$this->add_data('CopyrightYear',date('Y'));
+	}
+
+	protected function _setWpFooter()
+	{
+		$this->add_data('wpFooter',$this->_captureOutput('wp_footer'));
+	}
+}
