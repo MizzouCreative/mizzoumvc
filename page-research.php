0e7785868b96c23e0a7445b0beac305bd2d461ae
Paul Gilzow <gilzowp@missouri.edu>
2015-01-21 15:57:36 -0600

    documentation

--- page-research.php
+++ page-research.php
@@ -11,43 +11,7 @@ Template Name: Publications Archive
  * @category template
  * @author Charlie Triplett, University of Missouri
  * @copyright 2013 Curators of the University of Missouri
+ *
+ * @deprecated
+ * @todo delete
  */
-?>
-
-<?php get_header(); ?>
-
-<?php get_sidebar(); ?>
-
-	<main id="main" role="main">
-		
-			<article role="article"> 
-		
-				<?php if (have_posts()) : while (have_posts()) : the_post();?>
-		 		    
-					<div class="span6">
-
-						<?php if (function_exists('breadcrumbs')) breadcrumbs(); ?>
-
-						<div id="content">
-						    <h1 id="title"><?php the_title(); ?> <?php edit_post_link('Edit'); ?></h1>
-	
-							<section role="region" aria-label="content">
-		
-								<?php the_content(); ?>
-			
-								<?php include (TEMPLATEPATH . '/query-publications.php'); ?>
-		
-							</section>
-						</div> <!-- end content -->					
-					</div> <!-- end span6 -->
-					
-				<?php endwhile; endif;?>
-		
-			</article>
-		
-	
-	</main>
-
-
-
-<?php get_footer(); ?>
--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template file used to render sidebar
+ * Controller for sidebars
  * 
  * Called on other template files via 
  * <code>
@@ -15,28 +15,3 @@
  * @author Charlie Triplett, Web Communications, University of Missouri
  * @copyright 2013 Curators of the University of Missouri
  */
-?>
-
-<aside>
-
-	<div id="sidebar" class="mobile-hide right-offset2 span3">
-		<?php $themeta = get_post_custom($post_id); ?>
-		<?php  if ($themeta['childnav'][0] !='1') { //  if childnav is populated ?>
-			<?php if (function_exists('childnav')) childnav(); ?>
-		<?php  } ?>
-
-		
-		<?php if (is_single() || is_archive() && !is_tax() ) { ?>
-
-			<h2>Categories</h2>
-			<nav role="navigation">
-				<ol class="childnav">
-					<?php wp_list_categories('orderby=name&title_li='); ?>
-				</ol>
-			</nav>
-
-		<?php } ?>
-		
-	</div>
-
-</aside>
