fdee271ed43ecde701aaea049596c039eceb7a1f
Paul Gilzow <gilzowp@missouri.edu>
2015-02-02 14:41:10 -0600

    you can't get your processed search terms BEFORE you process them, dork!

--- search.php
+++ search.php
@@ -29,13 +29,12 @@ if ( (isset( $_GET['q'] ) && $_GET['q'] != '') || (isset($_GET['s']) && $_GET['s
     $arySearchData['objSite'] = $objSite;
     $objSearch = new Search($arySearchData);
 
+    $aryData['SearchResults'] = $objSearch->getSearchResults();
+    _mizzou_log($aryData['SearchResults'],'search results',false,array('file'=>__FILE__,'line'=>__LINE__));
+
     if($objSearch->SearchTerms != ''){
         $aryData['PageTitle'] = 'Search results for ' . htmlentities($objSearch->SearchTerms,ENT_QUOTES,'UTF-8',false);
     }
-
-    _mizzou_log($objSearch->SearchTerms,'search terms that were searched',false,array('file'=>__FILE__,'line'=>__LINE__));
-    $aryData['SearchResults'] = $objSearch->getSearchResults();
-    _mizzou_log($aryData['SearchResults'],'search results',false,array('file'=>__FILE__,'line'=>__LINE__));
 
     if($objSearch->isError()){
         _mizzou_log($objSearch->error_messages,'error messages from objSearch',false,array('file'=>__FILE__,'line'=>__LINE__));
