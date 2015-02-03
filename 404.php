7b4ef1db1d9103908018073bd5e61acb9e4be748
Paul Gilzow <gilzowp@missouri.edu>
2015-02-03 14:43:50 -0600

    changed to use 404 model

--- 404.php
+++ 404.php
@@ -15,35 +15,13 @@
  * @author Paul F. Gilzow & Jason L Rollins, Web Communications, University of Missouri
  * @copyright 2013 Curators of the University of Missouri
  */
+require_once 'models/FourOhFour.php';
 
+$aryData = array();
+$aryData['strRequestURI'] = $_SERVER['REQUEST_URI'];
+$aryData['objSite'] = new Site();
 
+$obj404 = new FourOhFour($aryData);
 
-$arySearchParams = unserialize(GSA_SEARCH_PARAMS); 
-$arySearchParams['q'] = mizzou_gather_404_search_terms();
-
-$strResults = file_get_contents(GSA_SEARCH_URL.http_build_query($arySearchParams));
-     
-$boolNoResults = (!isset($strResults) || $strResults == '') ? true : false; 
-get_header();
- ?>
- 	
-
-        <div id="default" class="span7">
-
-			<?php if (function_exists('breadcrumbs')) breadcrumbs(); ?>
-        
-            <?php get_template_part('aside','example'); ?>
-                    <h2>Not Found</h2>
-                    <p>
-                        The requested URL <?php echo htmlentities($_SERVER['REQUEST_URI'],ENT_QUOTES,'UTF-8');?> was not found on this site.  Here are the closest matches 
-                        I was able to find:
-                    </p>
-
-                    <?php if($boolNoResults): ?>
-                        <?php get_search_form(); ?>
-                    <?php else :?>
-                        <?php echo $strResults;?>
-                    <?php endif;?>
-        </div><!-- #default -->
-<?php get_footer(); ?>
+echo $obj404->getSearchResults();
 
