68c35b6acb9543466897bb3fa88f49c1a774872f
Paul Gilzow <gilzowp@missouri.edu>
2015-02-04 14:59:48 -0600

    initial commit

---
+++ Calendar.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template Name: Calendar
+ *
+ * @package 
+ * @subpackage 
+ * @since 
+ * @category 
+ * @category 
+ * @uses 
+ * @author Paul F. Gilzow, Web Communications, University of Missouri
+ * @copyright 2015 Curators of the University of Missouri
+ */
+require_once 'models/Calendar.php';
+
+$objSite = new Site();
+$objCalendar = new Calendar();
+$aryOptions = array(
+    'method'=>'department',
+    'term'  =>'4581'
+);
+
+echo $objCalendar->retrieveCalendarItems($aryOptions);
