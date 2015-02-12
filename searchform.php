d8fa367796aad69286431918ad48c93d1d600ad5
Paul Gilzow <gilzowp@missouri.edu>
2015-02-12 16:06:21 -0600

    documentation

--- searchform.php
+++ searchform.php
@@ -11,22 +11,9 @@
  * @category template-part
  * @author Charlie Triplett, Paul F. Gilzow & Jason L Rollins, Web Communications, University of Missouri
  * @copyright 2013 Curators of the University of Missouri
+ *
+ * @todo i dont think we need a controller for the search form.  just the view
  */
-    $arySearchParams = unserialize(GSA_SEARCH_PARAMS);
-?>
 
-<div class="search-container clearfix">
-	
-	<form id="search-form" action="/search/" method="get" role="search">
-	
-			<label class="hidden" for="search">Search</label>
-			<input id="search" class="search-field" name="q" size="22" type="text" onfocus="this.value=''" value="Search <?php bloginfo( 'name' ); ?> "/>
-		
-			<input class="search-button" name="sa" type="submit" value="Search"/> 
-		
-			<?php foreach($arySearchParams as $intKey=>$strValue):?>
-			<input name="<?php echo $intKey;?>" type="hidden" value="<?php echo $strValue;?>" />
-			<?php endforeach;?>
-	</form>
 
-</div>
+
