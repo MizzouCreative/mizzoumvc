784ae42092f82bdce2bb83f169057afd1db99c76
Paul Gilzow <gilzowp@missouri.edu>
2015-02-13 15:24:37 -0600

    why isnt objSite being passed through in aryContext?

--- footer.php
+++ footer.php
@@ -10,5 +10,7 @@
  * @copyright 2015 Curators of the University of Missouri
  */
 $objFooter = new Footer($aryContext);
+_mizzou_log($aryContext,'aryContext',false,array('line'=>__LINE__,'file'=>__FILE__));
 $aryData = array_merge($aryContext,$objFooter->getTemplateData());
+_mizzou_log($aryData,'aryData after merging with aryContext',false,array('line'=>__LINE__,'file'=>__FILE__));
 Content::render('footer',$aryData,array('include_header'=>false,'include_footer'=>false));
