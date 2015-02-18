16bb9cc64d8fc6517a30ca6a919166dbbfffea85
Paul Gilzow <gilzowp@missouri.edu>
2015-02-18 11:17:37 -0600

    debug no longer needed

--- models/MizzouPost.php
+++ models/MizzouPost.php
@@ -163,11 +163,11 @@ class MizzouPost extends PostBase
     {
         //we need the full pattern to use including the prefix, if applicable
         $strFullPattern = $this->_buildFullMetaGroupPattern($aryOptions['meta_prefix']);
-        _mizzou_log($strFullPattern,'the pattern I\'ll use to grep with',false,array('func'=>__FUNCTION__));
+        //_mizzou_log($strFullPattern,'the pattern I\'ll use to grep with',false,array('func'=>__FUNCTION__));
         //find all of the field keys that match our pattern
-        _mizzou_log(array_keys($this->aryOriginalCustomData),'keys from original custom data to grep on',false,array('line'=>__LINE__,'file'=>__FILE__));
+        //_mizzou_log(array_keys($this->aryOriginalCustomData),'keys from original custom data to grep on',false,array('line'=>__LINE__,'file'=>__FILE__));
         $aryMetaGroupKeys = preg_grep($strFullPattern,array_keys($this->aryOriginalCustomData));
-        _mizzou_log($aryMetaGroupKeys,'matches i found from the grep');
+        //_mizzou_log($aryMetaGroupKeys,'matches i found from the grep');
         //loop through each match, pull out the group component and add it the group array
         foreach($aryMetaGroupKeys as $strKeyInGroup){
             if(1 === preg_match($strFullPattern,$strKeyInGroup,$aryMatch)){
