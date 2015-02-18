e6bbf4d3b5e31e9132858d2faecd88cd3ce6b3cc
Paul Gilzow <gilzowp@missouri.edu>
2015-02-18 07:49:09 -0600

    people should always include meta

--- models/People.php
+++ models/People.php
@@ -16,4 +16,13 @@ require_once 'WpBase.php';
 
 class People extends WpBase{
     protected $strPostType = 'person';
+
+    public function __construct($strPostPreFix = null)
+    {
+        /**
+         * People should always include meta
+         */
+        $this->aryDefaults['include_meta'] = true;
+        parent::__construct($strPostPreFix);
+    }
 }
