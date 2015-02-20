ff872a27fdc7e616993231c037f4f28d0e5a1f45
Paul Gilzow <gilzowp@missouri.edu>
2015-02-20 08:13:28 -0600

    you were adding ^ twice

--- models/MizzouPost.php
+++ models/MizzouPost.php
@@ -64,7 +64,7 @@ class MizzouPost extends PostBase
      * @var string regex pattern for locating post members that should be consolidated into an array
      * ([a-zA-Z_]*)_?\d
      */
-    private $strMetaGroupPattern = '^\w*_?\d$';
+    private $strMetaGroupPattern = '\w*_?\d$';
 
     /**
      * @var string should be the default date format, but i think this is now deprecated, due to AP Style
