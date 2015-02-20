808732520d7c1056dcdbdc362775f73e68b04797
Paul Gilzow <gilzowp@missouri.edu>
2015-02-20 13:00:43 -0600

    initial commit

---
+++ sidenav.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * 
+ *
+ * @package 
+ * @subpackage 
+ * @since 
+ * @category 
+ * @category 
+ * @uses 
+ * @author Paul F. Gilzow, Web Communications, University of Missouri
+ * @copyright 2015 Curators of the University of Missouri
+ */
+
+$strMenu = '';
+
+if(isset($aryContext['menuName'])){
+    $strMenu = $aryContext['menuName'];
+} elseif(isset($aryContext['objMainPost'])) {
+    $aryAncestors = get_post_ancestors($objMainPost->ID);
+    if(count($aryAncestors) > 0){
+        $intOldestAncestor = end($aryAncestors);
+        $objOldestAncestor = get_post($intOldestAncestor);
+        $strMenu = $objOldestAncestor->post_title;
+    }
+}
+
+if($strMenu != ''){
+    $aryMenuOptions = array(
+        'menu' => $strMenu,
+        'echo' => false,
+    );
+
+    $strMenuContents = wp_nav_menu($aryMenuOptions);
+
+    $aryContex['menu'] = $strMenuContents;
+    Content::render('menu', $aryContex);
+
+
+}
