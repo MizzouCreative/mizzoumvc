2b26f5383868dd12ec6aaab98a96a091eb83d2d3
Paul Gilzow <gilzowp@missouri.edu>
2015-03-05 13:51:57 -0600

    blargh

--- models/Header.php
+++ models/Header.php
@@ -200,6 +200,7 @@ class Header extends Subview {
                 if($objChildNode->getAttribute('id') == 'menu-primary'){
                     $aryMainMenuLI = $objChildNode->getElementsByTagName('li');
                     $boolMatched = false;
+                    _mizzou_log(null,'currently',false,array('line'=>__LINE__,'file'=>basename(__FILE__)));
                     while((list($intChildKey,$objChildLI) = each($aryMainMenuLI)) && !$boolMatched){
                         _mizzou_log($objChildLI->nodeValue,'currently looping through main menu. child node value',false,array('line'=>__LINE__,'file'=>basename(__FILE__)));
                         if(trim($this->aryData['PageTitle']) == $objChildLI->nodeValue){
