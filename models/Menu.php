779822c01f5d0e448d7a80866ec9ffeac15c8540
Paul Gilzow <gilzowp@missouri.edu>
2015-03-11 14:40:38 -0500

    add primary menu injection

--- models/Menu.php
+++ models/Menu.php
@@ -27,6 +27,8 @@ class Menu extends Base {
 
     protected $aryMenuOptions = array();
 
+    protected $strPatternItemsWrap = '/^<((o|u)l)/';
+
     public function __construct($aryContext)
     {
         if(isset($aryContext['objSite'])){
@@ -50,6 +52,9 @@ class Menu extends Base {
 
             if( '' != $aryStaticMenus = $this->aryData['objSite']->option('static_menus')){
                 $this->_retrieveStaticMenus($aryStaticMenus);
+                if(isset($aryStaticMenus['Primary']) && ($this->aryData['objSite']->option('inject_primary') || 'yes' == $this->aryData['objSite']->option('inject_primary'))){
+                    $this->_injectPrimaryMenu();
+                }
             }
 
             $this->_retrievePageMenu();
@@ -101,11 +106,17 @@ class Menu extends Base {
 
     protected function _retrievePageMenu()
     {
-        $strMenu = $this->_determineMenuName();
-        _mizzou_log($strMenu,'name of the page menu im going to try and get',false,array('line'=>__LINE__,'file'=>basename(__FILE__)));
-        if($strMenu != ''){
-            $this->add_data('Page',$this->_retrieveMenu($strMenu));
+        /**
+         * it's possible we already determined the page menu when/if we injected into the primary
+         */
+        if(!isset($this->aryData['Page']) || $this->Page == ''){
+            $strMenu = $this->_determineMenuName();
+            _mizzou_log($strMenu,'name of the page menu im going to try and get',false,array('line'=>__LINE__,'file'=>basename(__FILE__)));
+            if($strMenu != ''){
+                $this->add_data('Page',$this->_retrieveMenu($strMenu));
+            }
         }
+
     }
 
     protected function _retrieveMenu($strMenuName)
@@ -113,4 +124,72 @@ class Menu extends Base {
         return wp_nav_menu(array_merge($this->aryMenuOptions,array('menu'=>$strMenuName)));
 
     }
+
+    protected function _determineListElementType()
+    {
+        $strReturn = 'ul';//wordpress default for menus
+        if('' != $strItemsWrap = $this->aryData['objSite']->option('items_wrap')){
+            if(1 === preg_match($this->strPatternItemsWrap,$strItemsWrap,$aryMatches)){
+                $strReturn = $aryMatches[1];
+            }
+        }
+
+        return $strReturn;
+    }
+
+    protected function _injectPrimaryMenu()
+    {
+        if($this->aryData['PageTitle'] != ''){ // if we have no page title, then there isnt a menu to inject
+            $objDomMenu = new DOMDocument();
+            $objDomMenu->loadXML($this->aryData['Primary']);
+            //_mizzou_log($objDomMenu->saveHTML(),'our menu as a DOMobject',false,array('line'=>__LINE__,'file'=>basename(__FILE__)));
+            $strListElementType = $this->_determineListElementType();
+            $objOLNodes = $objDomMenu->getElementsByTagName($strListElementType);
+            //_mizzou_log(null,'currently',false,array('line'=>__LINE__,'file'=>basename(__FILE__)));
+            for($j=0;$j<$objOLNodes->length;++$j){
+                //_mizzou_log(null,'currently',false,array('line'=>__LINE__,'file'=>basename(__FILE__)));
+                $objChildNode = $objOLNodes->item($j);
+                if($objChildNode->getAttribute('id') == 'menu-primary'){
+                    $j = $objOLNodes->length;
+                    $objMainMenuLIList = $objChildNode->getElementsByTagName('li');
+                    for($i=0;$i<$objMainMenuLIList->length;++$i){
+                        $objChildLI = $objMainMenuLIList->item($i);
+                        if(trim($this->aryData['PageTitle']) == $objChildLI->nodeValue){
+                            $i = $objMainMenuLIList->length;
+                            /* ok, so we have a node title that matches a page title. now let's go see if we have a
+                             * matching menu
+                             */
+                            /**
+                             * @todo should we attempt to have an option for the menu class to be inserted here? injection_class in menu_options of the config file?
+                             */
+                            $aryMenuOptions = array_merge($this->aryMenuOptions,array('menu' => $objChildLI->nodeValue,));
+
+                            if('' != $strSubMenu = wp_nav_menu($aryMenuOptions)){
+                                //_mizzou_log($strSubMenu,'we found an element that matches our current page! Here is our matching menu',false,array('line'=>__LINE__,'file'=>basename(__FILE__)));
+                                //store it in the site object
+                                $this->add_data('Page',$strSubMenu);
+                                $objDomSecondaryMenu = new DOMDocument();
+                                $objDomSecondaryMenu->loadXML($strSubMenu);
+                                $objSecondaryMenuNode = $objDomSecondaryMenu->getElementsByTagName('ul')->item(0);
+                                /*
+                                $objFirstChildOfSecondMenu = $objSecondaryMenuNode->firstChild;
+                                _mizzou_log($objFirstChildOfSecondMenu->nodeValue,'what is the first child of our second menu?');
+                                */
+
+                                $objChildLI->appendChild($objDomMenu->importNode($objSecondaryMenuNode,true));
+                                $this->add_data('Primary',$objDomMenu->saveHTML());
+                                //_mizzou_log($this->aryData['objSite']->PrimaryMenu,'our menu in html after injection',false,array('line'=>__LINE__,'file'=>basename(__FILE__)));
+
+                            }
+                        }
+                    }
+                }
+
+
+            }
+
+        }
+
+    }
+
 }
