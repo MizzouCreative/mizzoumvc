6844d67df04f71560c8bca89282fef6199208917
Paul Gilzow <gilzowp@missouri.edu>
2015-03-30 08:52:17 -0500

    add _apmonth and date properties

--- models/Calendar.php
+++ models/Calendar.php
@@ -35,13 +35,40 @@ class Calendar extends AbstractTranslator {
         $objReturn->Id              = $objEvent->id;
         $objReturn->Title           = (string)$objEvent->title;
         $objReturn->Description     = (string)$objEvent->description;
-        $objReturn->RoomNumber     = (string)$objEvent->room_number;
-        $objReturn->LocationName   = (string)$objEvent->location_name;
+        $objReturn->RoomNumber      = (string)$objEvent->room_number;
+        $objReturn->LocationName    = (string)$objEvent->location_name;
         $objReturn->Url             = (string)$objEvent->localist_url;
-        $objReturn->DescriptionText= (string)$objEvent->description_text;
+        $objReturn->DescriptionText = (string)$objEvent->description_text;
         $objReturn->Start           = strtotime((string)$objEvent->event_instances[0]->event_instance->start);
         $objReturn->End             = strtotime((string)$objEvent->event_instances[0]->event_instance->end);
+	    $objReturn->StartMonth      = date('M',$this->Start);
+	    $objReturn->StartAPMonth    = $this->_getAPMonth($objReturn->StartMonth);
+	    $objReturn->EndAPMonth      = $this->_getAPMonth(($objReturn->EndMonth));
+	    $objReturn->EndMonth        = date('M',$this->End);
+	    $objReturn->StartDay        = date('j',$this->Start);
+	    $objReturn->StartYear       = date('Y',$this->Start);
+	    $objReturn->EndYear         = date('Y',$this->End);
 
         return $objReturn;
     }
+
+	/**
+	 * Reformats the Month to AP style date format based on the timestamp of the post
+	 * @return string AP style formatted month
+	 * @todo this is the exact same function from @see MizzouPost::_getAPMonth(). Can we convert
+	 */
+	private function _getAPMonth($strMonth)
+	{
+		if(strlen($strMonth) > 5){ //stoopid september... grumble, grumble
+			if($strMonth == 'September'){
+				$intTruncLen = 4;
+			} else {
+				$intTruncLen = 3;
+			}
+
+			$strMonth = substr($strMonth,0,$intTruncLen) . '.';
+		}
+
+		return $strMonth;
+	}
 }
