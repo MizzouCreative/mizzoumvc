745f1743ffce9793cfbb49226399b36d2e426fd8
Paul Gilzow <gilzowp@missouri.edu>
2015-04-14 16:52:26 -0500

    header stuff

--- MizzouMVC.php
+++ MizzouMVC.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Plugin Name: MizzouMVC
- * Plugin URI: http://webcom.missouri.edu/
+ * Plugin URI: http://universityaffairs.missouri.edu/department/web-communications/
  * Description: MVC Framework for rapid deployment of custom themes
  * Version: v1.1
- * Author Paul F. Gilzow, Web Communications, University of Missouri
- *
+ * Author: Paul F. Gilzow, Web Communications, University of Missouri
+ * Author URI: http://universityaffairs.missouri.edu/department/web-communications/
  * @package 
  * @subpackage 
  * @since 
