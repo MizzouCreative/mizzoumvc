280663d1ff78a8de4b0a064a5c48e94fed8868df
Paul Gilzow <gilzowp@missouri.edu>
2015-04-14 16:46:17 -0500

    initial commit

---
+++ MizzouMVC.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: MizzouMVC
+ * Plugin URI: http://webcom.missouri.edu/
+ * Description: MVC Framework for rapid deployment of custom themes
+ * Version: v1.1
+ * Author Paul F. Gilzow, Web Communications, University of Missouri
+ *
+ * @package 
+ * @subpackage 
+ * @since 
+ * @category 
+ * @category 
+ * @uses 
+ * @author Paul F. Gilzow, Web Communications, University of Missouri
+ * @copyright 2015 Curators of the University of Missouri
+ */
