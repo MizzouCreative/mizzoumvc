078663923780a28e7038f61ad6a6cacaec6276ef
Paul Gilzow <gilzowp@missouri.edu>
2015-04-16 11:17:18 -0500

    timezone hack

--- Calendar.php
+++ Calendar.php
@@ -19,6 +19,11 @@ $arySiteCalendarOptions = $objSite->calendar;
 //do we need to include the excerpt_length option when creating our calendar object?
 $aryCalendarOptions = (isset($arySiteCalendarOptions['excerpt_length'])) ? array('excerpt_length'=>$arySiteCalendarOptions['excerpt_length']) : array();
 
+/**
+ * @todo this is a hack.  push these changes back up to the Abstract class for the Calendar
+ */
+$strOldTimeZone = date_default_timezone_get();
+date_default_timezone_set('America/Chicago');
 $objCalendar = new Calendar($aryCalendarOptions);
 
 if(isset($arySiteCalendarOptions['method']) && isset($arySiteCalendarOptions['term'])){
@@ -30,5 +35,5 @@ if(isset($arySiteCalendarOptions['method']) && isset($arySiteCalendarOptions['te
 	_mizzou_log($arySiteCalendarOptions,'you asked me to get calendar items, but it doesnt appear you gave me the data i need.',false,array('line'=>__LINE__,'file'=>__FILE__));
 	$aryData['Events'] = array();
 }
-
+date_default_timezone_set($strOldTimeZone);
 Content::render('calendar',$aryData);
