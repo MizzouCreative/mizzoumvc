7a2be73ca0611918699e9c46ef48820685f40e82
Paul Gilzow <gilzowp@missouri.edu>
2015-04-20 12:01:53 -0500

    further changes on dealing with timezones

--- Calendar.php
+++ Calendar.php
@@ -14,26 +14,21 @@
 require_once 'models/Calendar.php';
 
 $objSite = new Site();
-$arySiteCalendarOptions = $objSite->calendar;
-
-//do we need to include the excerpt_length option when creating our calendar object?
-$aryCalendarOptions = (isset($arySiteCalendarOptions['excerpt_length'])) ? array('excerpt_length'=>$arySiteCalendarOptions['excerpt_length']) : array();
+$aryCalendarOptions = $objSite->calendar;
 
 /**
  * @todo this is a hack.  push these changes back up to the Abstract class for the Calendar
  */
-$strOldTimeZone = date_default_timezone_get();
-date_default_timezone_set('America/Chicago');
+//$strOldTimeZone = date_default_timezone_get();
+//date_default_timezone_set('America/Chicago');
 $objCalendar = new Calendar($aryCalendarOptions);
 
-if(isset($arySiteCalendarOptions['method']) && isset($arySiteCalendarOptions['term'])){
-	$aryOptions = $arySiteCalendarOptions;
-	unset($aryOptions['excerpt_length']);
-	$aryReturn = $objCalendar->retrieveCalendarItems($aryOptions);
+if(isset($aryCalendarOptions['method']) && isset($aryCalendarOptions['term'])){
+	$aryReturn = $objCalendar->retrieveCalendarItems($aryCalendarOptions);
 	$aryData['Events'] = $aryReturn['events'];
 } else {
-	_mizzou_log($arySiteCalendarOptions,'you asked me to get calendar items, but it doesnt appear you gave me the data i need.',false,array('line'=>__LINE__,'file'=>__FILE__));
+	_mizzou_log($aryCalendarOptions,'you asked me to get calendar items, but it doesnt appear you gave me the data i need.',false,array('line'=>__LINE__,'file'=>__FILE__));
 	$aryData['Events'] = array();
 }
-date_default_timezone_set($strOldTimeZone);
+//date_default_timezone_set($strOldTimeZone);
 Content::render('calendar',$aryData);
