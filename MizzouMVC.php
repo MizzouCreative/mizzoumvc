0da79662cbe841fb6e8d9b50ab458b271491f022
Paul Gilzow <gilzowp@missouri.edu>
2015-05-04 13:16:41 -0500

    testing out using a CPT as a settings option

--- MizzouMVC.php
+++ MizzouMVC.php
@@ -20,23 +20,57 @@ define('MIZZOUMVC_ROOT_URL',plugins_url('',__FILE__));
 require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'functions'.DIRECTORY_SEPARATOR.'template-locator.php';
 require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'functions.php';
 
-add_action('admin_menu','mizzoumvcRegisterAdminMenu');
-add_action('admin_menu','mizzoumvcRegisterThemeAdminMenu');
+//add_action('admin_menu','mizzoumvcRegisterAdminMenu');
+//add_action('admin_menu','mizzoumvcRegisterThemeAdminMenu');
+add_action('after_setup_theme','mizzouMVCShouldWeRegisterSettingsCPT');
 
-function mizzoumvcRegisterAdminMenu(){
+function mizzoumvcRegisterAdminMenu()
+{
     add_options_page('MizzouMVC Settings','MizzouMVC','manage_options','mizzoumvc','mizzoumvcAdminMenuTest');
 }
 
-function mizzoumvcRegisterThemeAdminMenu(){
+function mizzoumvcRegisterThemeAdminMenu()
+{
     if(defined('MIZZOUMVC_COMPATIBLE') && MIZZOUMVC_COMPATIBLE){
         add_theme_page('Theme Settings','Theme Settings','edit_theme_options','mizzoumvc-theme','mizzoumvcThemeSettings');
     }
 }
 
-function mizzoumvcAdminMenuTest(){
+function mizzoumvcAdminMenuTest()
+{
     echo 'MizzouMVC Settings Test';
 }
 
-function mizzoumvcThemeSettings(){
+function mizzoumvcThemeSettings()
+{
     echo 'MizzouMVC Theme Settings Test';
+}
+
+function mizzouMVCShouldWeRegisterSettingsCPT()
+{
+    if(defined('MIZZOUMVC_COMPATIBLE') && MIZZOUMVC_COMPATIBLE){
+        add_action('init','mizzouMVCRegisterSettingsCPT');
+    }
+}
+
+function mizzouMVCRegisterSettingsCPT()
+{
+    mizzouRegisterPostType('mizzoumvc-settings',array(
+        'public'=>false,
+        'has_archive'=>false,
+        'show_ui'=>true,
+        'show_in_nav_menus'=>true,
+        'show_in_menu'=>'themes.php',
+        'menu_position'=>null,
+        'supports'=>array(
+            'title',
+            'custom-fields',
+            'page-attributes'
+        ),
+        'rewrite'=>false,
+        'query_var'=>false,
+
+
+
+    ));
 }
