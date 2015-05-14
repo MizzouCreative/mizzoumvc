021d43314eb60879ee7601e6eae8888f5b1bfb64
Paul Gilzow <gilzowp@missouri.edu>
2015-05-14 12:02:12 -0500

    normally we want to hand the args from wordpress over to the called function, but in some instances, wordpress doesn't send over any data. added an override option to the constructor so we can instruct the __call method to use the data we pass in instead.

--- library/DynamicHook.php
+++ library/DynamicHook.php
@@ -14,15 +14,26 @@
 
 class DynamicHook {
     private $mxdData;
+    private $aryOptions = array(
+        'override'=>false,
+    );
 
-    public function __construct($mxdArgs)
+    public function __construct($mxdArgs,$aryOptions=array())
     {
         $this->mxdData = $mxdArgs;
+        if(is_array($aryOptions) && count($aryOptions) > 0){
+            $this->aryOptions = array_merge($this->aryOptions,$aryOptions);
+        }
     }
 
     public function __call($strCallBack,$mxdArgs)
     {
         try {
+            if($this->aryOptions['override']){
+                $mxdArgs = $this->mxdData;
+                $this->mxdData = null;
+            }
+
             return call_user_func($strCallBack,$mxdArgs,$this->mxdData);
         } catch (InvalidArgumentException $objError) {
             _mizzou_log($strCallBack,$objError->getMessage(),false,array('line'=>__LINE__,'file'=>basename(__FILE__)));
