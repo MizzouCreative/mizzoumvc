861b78243d14427c2739dedd10bb68d8905ed549
Paul Gilzow <gilzowp@missouri.edu>
2015-07-28 15:44:41 -0500

    method definition didnt match parent def

--- library/A11yPageWalker.php
+++ library/A11yPageWalker.php
@@ -13,12 +13,12 @@
 
 class A11yPageWalker extends Walker_Page {
 
-    function start_lvl(&$output, $depth, $aryArgs = array())
+    function start_lvl(&$output, $depth = 0, $aryArgs = array())
     {
         $strIndent = str_repeat("\t", $depth);
         $output .= PHP_EOL.$strIndent.'<ol class="children">'.PHP_EOL;
     }
-    function end_lvl(&$output, $depth, $aryArgs = array())
+    function end_lvl(&$output, $depth = 0, $aryArgs = array())
     {
         $strIndent = str_repeat("\t", $depth);
         $output .= $strIndent.'</ol>'.PHP_EOL;
