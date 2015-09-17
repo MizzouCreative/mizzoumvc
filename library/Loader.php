2e9c796b7a54f231bc271786627f151200304740
Paul Gilzow <gilzowp@missouri.edu>
2015-09-17 12:58:01 -0500

    initial commit

---
+++ library/Loader.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * 
+ *
+ * @package 
+ * @subpackage 
+ * @since 
+ * @category 
+ * @category 
+ * @uses 
+ * @author Paul F. Gilzow, Web Communications, University of Missouri
+ * @copyright 2015 Curators of the University of Missouri
+ */
+
+namespace MizzouMVC\library;
+
+
+class Loader {
+
+    protected $strFrameworkPath = null;
+    protected $strParentThemePath = null;
+    protected $strChildThemePath = null;
+
+    public function _construct($strFramework,$strParentTheme,$strChildTheme=null)
+    {
+        $this->strFrameworkPath = $strFramework;
+        $this->strParentThemePath = $strParentTheme;
+        if(!is_null($strChildTheme) || $strParentTheme != $strChildTheme){
+            $this->strChildThemePath = $strChildTheme;
+        }
+    }
+
+    public function load($strClass)
+    {
+        /*
+         * first lets see if they gave us the file instead of the class. if so, we'll assume they named the file the same
+         * as the class name
+         */
+        if(1 === preg_match('/\.[a-z]{1,5}$/',$strClass,$aryMatch)){
+            _mizzou_log($strClass,'you gave me a file, but i need the class name',false,array('file'=>__FILE__,'line'=>__LINE__));
+            $strClass = $aryMatch[1];
+        }
+
+        $aryClassParts = explode('\\',$strClass);
+
+        $strFullPath = $this->_determinePath($aryClassParts) . end($aryClassParts) . '.php';
+
+        if(file_exists($strFullPath)){
+            require_once $strFullPath;
+            if(class_exists($strClass,false)){
+                return new $strClass;
+            } else {
+                _mizzou_log($strClass,'the class name you gave me doesnt exist. make sure the namespace is correct',false,array('line'=>__LINE__,'file'=>__FILE__));
+            }
+        } else {
+            _mizzou_log($strClass,'the class name you gave me doesnt map to a file. file map: ' . $strFullPath,false,array('line'=>__LINE__,'file'=>__FILE__));
+        }
+    }
+
+    protected function _determinePath($aryClassParts)
+    {
+        $strFullPath = '';
+
+        $strEndPiece = end($aryClassParts);
+
+        //reset and grab the first piece
+        $strFirst = reset($aryClassParts);
+
+        if('MizzouMVC' == $strFirst){
+            $strFullPath = $this->strFrameworkPath;
+        } else {
+            $strSecond = next($aryClassParts);
+            //the first part then will be the project root. The second part COULD be the parent/child indicator, but if
+            //not, then it'll be the "parent" path
+            if('child' == $strSecond){
+                $strFullPath = $this->strChildThemePath;
+            } else {
+                $strFullPath = $this->strParentThemePath;
+            }
+        }
+
+        //ok, now we have our root start
+        $strNext = next($aryClassParts);
+        if('controllers' != $strNext){
+            //ok, add the strNext, then loop through the remainder of the pieces
+            while(list($intKey,$strNext) = each($aryClassParts)){
+                if($strEndPiece != $strNext){
+                    $strFullPath .= $strNext . DIRECTORY_SEPARATOR;
+                }
+            }
+        }
+
+        return $strFullPath;
+    }
+}
