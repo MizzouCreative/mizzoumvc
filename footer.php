5b78094106dbf4a4c2e3c964d556bb2baaa9de94
Paul Gilzow <gilzowp@missouri.edu>
2015-09-18 15:07:50 -0500

    switch to extending Main

--- footer.php
+++ footer.php
@@ -9,6 +9,19 @@
  * @author Paul F. Gilzow, Web Communications, University of Missouri
  * @copyright 2015 Curators of the University of Missouri
  */
+namespace MizzouMVC\controllers;
+
+class Footer extends Main {
+    public function main()
+    {
+        $objFooter = $this->load('MizzouMVC\models\Footer',$this->aryRenderData);
+        $this->aryRenderData = array_merge($this->aryRenderData,$objFooter->getTemplateData);
+        $this->render('footer');
+    }
+}
+
+$objFooter = new Footer((isset($aryContext) ? $aryContext : array()));
+/**
 $objFooter = new Footer($aryContext);
 $aryData = array_merge($aryContext,$objFooter->getTemplateData());
-Content::render('footer',$aryData,array('include_header'=>false,'include_footer'=>false));
+Content::render('footer',$aryData,array('include_header'=>false,'include_footer'=>false));*/
