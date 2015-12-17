5152101bb4b037f6dea72ae75a0b0eb7874f7a76
Paul Gilzow <gilzowp@missouri.edu>
2015-12-17 15:29:08 -0600

    documentation

--- header.php
+++ header.php
@@ -1,6 +1,8 @@
 <?php
 /**
- * Template file used to contain header content of theme pages
+ * Controller file used to gather components for header view
+ *
+ * Do NOT instantiate the class.  The main controller will do that
  *
  * @package WordPress
  * @subpackage MizzouMVC
@@ -9,31 +11,6 @@
  * @author Paul F. Gilzow, Web Communications, University of Missouri
  * @copyright 2015 Curators of the University of Missouri
  *
- * If called via a template, will have access to $aryContext which contains all variables given to the templating engine
- *
- * Will need to gather
- *  - <title> contents: $strHeadTitle
- *  - template location: see $objSite below
- *  - site option for tracking code: see $objSite below
- *  - blog url: see $objSite below
- *  - blog name: see $objSite below
- *  - navigation menus: see $objSite below
- *  - search form: $strSearchFormContents
- *  - contents of wp_head: $strWpHeaderContents
- *  - whether or not to include <meta> robots: $boolIncludeNoIndex
- *
- *  $objSite contains
- *  -> CopyrightYear (also accessible as $strCopyrightYear)
- *  -> Name (also accessible as $strSiteName)
- *  -> URL (also accessible as $strSiteURL)
- *  -> ParentThemeURL (also accessible as $strParentThemeURL)
- *  -> ChildThemeURL (also accessible as $strChildThemeURL)
- *  -> ActiveStylesheet (also accessible as $strActiveStylesheet)
- *  -> ActiveThemeURL (also accessible as $strActiveThemeURL)
- *  -> TrackingCode (also accessible as $strTrackingCode)
- *  -> PrimaryMenu (also accessible as $strPrimaryMenu)
- *  -> AudienceMenu (also accessible as $strAudienceMenu)
- *  -> LastModifiedDate (also accessible as $strModifiedDate)
  *
  */
 namespace MizzouMVC\controllers;
