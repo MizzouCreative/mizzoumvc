b3bae9eb84ae7faf4d3c892715fa5624a3aecdb8
Paul Gilzow <gilzowp@missouri.edu>
2015-12-17 13:01:29 -0600

    convert to extending Main controller class

--- Calendar.php
+++ Calendar.php
@@ -11,19 +11,31 @@
  * @author Paul F. Gilzow, Web Communications, University of Missouri
  * @copyright 2015 Curators of the University of Missouri
  */
-require_once 'models/Calendar.php';
+namespace MizzouMVC\controllers;
 
-$objSite = new Site();
-$aryCalendarOptions = $objSite->calendar;
+class Calendar extends Main {
 
-$objCalendar = new Calendar($aryCalendarOptions);
+    public function main()
+    {
+        if(isset($this->objSite->calendar) && is_array($this->objSite->calendar)){
+            $aryCalendarOptions =  $this->objSite->calendar;
+            $objCalendar = $this->load('MizzouMVC\models\Calendar',$aryCalendarOptions);
 
-if(isset($aryCalendarOptions['method']) && isset($aryCalendarOptions['term'])){
-	$aryReturn = $objCalendar->retrieveCalendarItems($aryCalendarOptions);
-	$aryData['Events'] = $aryReturn['events'];
-} else {
-	_mizzou_log($aryCalendarOptions,'you asked me to get calendar items, but it doesnt appear you gave me the data i need.',false,array('line'=>__LINE__,'file'=>__FILE__));
-	$aryData['Events'] = array();
+            if(isset($aryCalendarOptions['method']) && isset($aryCalendarOptions['term'])){
+                $aryReturn = $objCalendar->retrieveCalendarItems($aryCalendarOptions);
+                $aryEvents = $aryReturn['events'];
+            } else {
+                _mizzou_log($aryCalendarOptions,'you asked me to get calendar items, but it doesnt appear you gave me the data i need.',false,array('line'=>__LINE__,'file'=>__FILE__));
+                $aryEvents = array();
+            }
+
+            $this->renderData('Events',$aryEvents);
+        } else {
+            _mizzou_log($this->objSite,'you asked me for calendar stuff but there doesnt appear to be any calendar settings',false,array('line'=>__LINE__,'file'=>__FILE__));
+        }
+
+        $this->render('calendar');
+    }
 }
 
-Content::render('calendar',$aryData);
+new Calendar();
