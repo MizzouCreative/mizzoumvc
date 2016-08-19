cd6ac6d988b292e3f26b0a08591be68868e17124
Paul Gilzow <gilzowp@missouri.edu>
2016-08-19 15:05:10 -0500

    accommodate internal search

--- search.php
+++ search.php
@@ -24,27 +24,61 @@ class Search extends Main
      */
     public function main()
     {
-        if ( (isset( $_GET['q'] ) && $_GET['q'] != '') || (isset($_GET['s']) && $_GET['s'] != '')) {
-            $arySearchData = array();
-            $arySearchData['arySearchParams'] = $_GET;
-            if(isset($this->objSite->search) && is_array($this->objSite->search)){
-                $arySearchData['search_options'] = $this->objSite->search;
+        $strPageTitle = 'Search Results for ';
+        $strSearchTerms = '';
+
+
+        if($this->objSite->search['internal']){
+            if(isset($_GET['s']) && '' !== $_GET['s']){
+                $this->boolIncludePagination = true;
+                $objWpBase = $this->load('MizzouMVC\models\WpBase');
+                if(is_search()){
+                    //we're on the search results page and wordpress should already have our search results preppped
+                    $mxdSearchResults = $objWpBase->convertPosts($this->wp_query->posts);
+                    $strSearchTerms = get_search_query();
+                } else {
+                    //so we need to perform the search manually
+                    $aryOptions = array(
+                        'post_type' => '',//all post types. @todo should we worry about limiting to specific post types?
+                        'passthru'  => array('s'=>$_GET['s']),//@todo do we need to validate this before passing? WP_QUERY (which is ultimately used by WpBase) should santize it for us
+                    );
+                    $strSearchTerms = $_GET['s']; //escaped before passing to the view
+                    $mxdSearchResults = $objWpBase->retrieveContent($aryOptions);
+                }
+            } else {
+                //so what now?
+            }
+        } else {
+
+            if( (isset( $_GET['q'] ) && $_GET['q'] != '') || (isset($_GET['s']) && $_GET['s'] != '')){
+                $arySearchData = array(
+                    'search_options' => $this->objSite->search,
+                    'search_parameters' => $_GET,
+                );
+                $arySearchData['arySearchParams'] = $_GET;
                 $objSearch = $this->load('MizzouMVC\models\Search',$arySearchData);
-                $this->renderData('SearchResults',$objSearch->getSearchResults());
+                $mxdSearchResults = $objSearch->getSearchResults();
                 if('' != $objSearch->SearchTerms){
-                    $this->renderData('SearchTerms',htmlentities($objSearch->SearchTerms,ENT_QUOTES,'UTF-8',false));
-                    $this->renderData('PageTitle','Search Results for ' . $this->aryRenderData['SearchTerms']);
+                    $strSearchTerms = $objSearch->SearchTerms;
                 }
 
                 if($objSearch->isError()){
                     _mizzou_log($objSearch->error_messages,'error messages from objSearch',false,array('file'=>__FILE__,'line'=>__LINE__));
                 }
             } else {
-                _mizzou_log($this->objSite,'trying to do a search, but search options are missing',false,array('line'=>__LINE__,'file'=>__FILE__));
+                /**
+                 * @todo why are we resetting the page title back to what it is anyway from wordpress? rethink the logic
+                 * here on how we handle page title
+                 */
+                $strPageTitle = 'Search';
             }
-
         }
 
+        $strSearchTerms = esc_attr($strSearchTerms);
+
+        $this->renderData('SearchResults',$mxdSearchResults);
+        $this->renderData('SearchTerms',$strSearchTerms);
+        $this->renderData('PageTitle',$strPageTitle . $strSearchTerms);
         $this->render('search');
     }
 }
