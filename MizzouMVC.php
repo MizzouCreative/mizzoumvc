55de49d19528311b36a0b6ac92150572f32d2a75
Paul Gilzow <gilzowp@missouri.edu>
2016-09-01 10:41:57 -0500

    missed a version number, updating to force kernl to repush

--- MizzouMVC.php
+++ MizzouMVC.php
@@ -3,7 +3,7 @@
  * Plugin Name: MizzouMVC
  * Plugin URI: https://marcom.missouri.edu/department/miz-creative/
  * Description: MVC Framework for rapid custom theme development
- * Version: 3.5.0
+ * Version: 3.5.1
  * Author: Paul F. Gilzow, Mizzou Creative, University of Missouri
  * Author URI: https://marcom.missouri.edu/department/miz-creative/
  * @package W
@@ -14,12 +14,12 @@
  * @uses 
  * @author Paul F. Gilzow, Mizzou Creative, University of Missouri
  * @copyright 2016 Curators of the University of Missouri
- * @version 3.5.0
+ * @version 3.5.1
  */
 /**
  * @todo let's check to see if the memory is low and then increase if needed
  */
-define('MIZZOUMVC_VERSION','3.5.0');
+define('MIZZOUMVC_VERSION','3.5.1');
 define('MIZZOUMVC_ROOT_PATH',dirname(__FILE__).DIRECTORY_SEPARATOR);
 define('MIZZOUMVC_ROOT_URL',plugins_url('',__FILE__));
 require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'functions'.DIRECTORY_SEPARATOR.'template-locator.php';
--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 Contributors: gilzow, metzenj, nicholsjc
 Requires at least: 4.1
 Tested up to: 4.6
-Stable tag: 3.5.0
+Stable tag: 3.5.1
 License: GPLv2 or later
 Tags: Framework, MVC, theme development
 
@@ -33,7 +33,7 @@ In All Theme Settings --> Site Wide, add two new Custom Fields
 * use_framework_stylesheet - set to 'yes' if you want to use the stylesheet from the framework instead of your style.css file; framework assumes 'no'
 
 == Changelog ==
-= 3.5.0 =
+= 3.5.1 =
 * Changed label for theme settings area from "All Settings" to "All Theme Settings"
 * Added ability to use internal search OR external search for both search and 404 since not everyone has a GSA.
 * new markup in the default views + updated css
