14e26110078c98b16dd5634275d7065bd688fe96
Paul Gilzow <gilzowp@missouri.edu>
2019-09-09 14:57:28 -0500

    Updates Twig version to 1.42.2 from 1.18.2. Updates version number to 3.9.0 Adds changelog entries

--- MizzouMVC.php
+++ MizzouMVC.php
@@ -14,12 +14,12 @@
  * @uses
  * @author Paul F. Gilzow, Mizzou Creative, University of Missouri
  * @copyright 2016 Curators of the University of Missouri
- * @version 3.8.3
+ * @version 3.9.0
  */
 /**
  * @todo let's check to see if the memory is low and then increase if needed
  */
-define('MIZZOUMVC_VERSION','3.8.3');
+define('MIZZOUMVC_VERSION','3.9.0');
 define('MIZZOUMVC_ROOT_PATH',dirname(__FILE__).DIRECTORY_SEPARATOR);
 define('MIZZOUMVC_ROOT_URL',plugins_url('',__FILE__));
 require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'functions'.DIRECTORY_SEPARATOR.'template-locator.php';
